659344af9b2c74394decd509dbb6d8760f92c61b
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-04 18:12:53 +0100

    Accurate connection replaced by synchronization

--- migasfree/client/views/__init__.py
+++ migasfree/client/views/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from .safe import SafeComputerViewSet, SafeAccurateConnectionView
+from .safe import SafeComputerViewSet, SafeSynchronizationView
 from .public import PackagerKeysView, ProjectKeysView
 from .token import (
     ComputerViewSet, ErrorViewSet, FaultViewSet,
