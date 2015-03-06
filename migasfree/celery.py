dd35edc551cd5d7d3d36a0bc646e4cd7f269a702
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-06 14:45:15 +0100

    Added celery

---
+++ migasfree/celery.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+from django.conf import settings
+
+# FIXME development or production
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migasfree.settings.development')
+
+app = Celery('migasfree')
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS) #, related_name="deferred")
