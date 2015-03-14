4ac50c048159797896245fb72410604d38307ca0
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-14 18:58:58 +0100

    Added SafeHardwareViewSet

--- migasfree/hardware/views.py
+++ migasfree/hardware/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+# -*- coding: utf-8 *-*
 
-# Create your views here.
+# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import viewsets, status
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+
+from migasfree.core.mixins import SafeConnectionMixin
+from migasfree.client.models import Computer
+from migasfree.utils import trans
+
+from .models import Node
+from . import tasks
+
+
+class SafeHardwareViewSet(SafeConnectionMixin, viewsets.ViewSet):
+    @list_route(methods=['post'])
+    def hardware(self, request, format=None):
+        """
+        claims = {
+            'id': computer_id,
+            'hardware': json_structure,
+        }
+        """
+
+        claims = self.get_claims(request.data)
+        if not claims or 'id' not in claims or 'hardware' not in claims:
+            return Response(
+                self.create_response(trans('Malformed claims')),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        computer = get_object_or_404(Computer, id=claims.get('id'))
+
+        Node.objects.filter(computer=computer).delete()
+        tasks.save_computer_hardware.delay(computer.id, claims.get('hardware'))
+        computer.update_last_hardware_capture()
+
+        return Response(
+            self.create_response(trans('Data received')),
+            status=status.HTTP_200_OK
+        )
