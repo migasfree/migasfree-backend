02701924bdb7c894e659570b61d66b187b01562f
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-18 18:03:44 +0100

    Create directories for target file (handle_uploaded_file)

--- migasfree/core/models/package.py
+++ migasfree/core/models/package.py
@@ -94,6 +94,10 @@ class Package(models.Model):
 
     @staticmethod
     def handle_uploaded_file(f, target):
+        path = os.path.dirname(target)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
         with open(target, 'wb+') as destination:
             for chunk in f.chunks():
                 destination.write(chunk)
