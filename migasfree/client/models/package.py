7359ea87799f667d5ae2494585dbbda1539b1b11
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-20 13:16:41 +0100

    Changed version max length to 40

--- migasfree/client/models/package.py
+++ migasfree/client/models/package.py
@@ -58,7 +58,7 @@ class Package(models.Model):
 
     version = models.CharField(
         verbose_name=_('version'),
-        max_length=20,
+        max_length=40,
         null=False,
         unique=False
     )
