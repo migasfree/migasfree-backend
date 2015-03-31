ef731260c4b2637301eb51ade2ca79ccf827f18c
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-31 21:23:46 +0200

    Removed permissions

--- migasfree/device/models/manufacturer.py
+++ migasfree/device/models/manufacturer.py
@@ -1,25 +1,40 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Manufacturer(models.Model):
     name = models.CharField(
-        _("name"),
+        verbose_name=_("name"),
         max_length=50,
         null=True,
         blank=True,
         unique=True
     )
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
     class Meta:
         app_label = 'device'
         verbose_name = _("Device (Manufacturer)")
         verbose_name_plural = _("Device (Manufacturers)")
-        permissions = (
-            ("can_save_device_manufacturer", "Can save Device Manufacturer"),
-        )
