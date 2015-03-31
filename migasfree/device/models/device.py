daa4ad4353e2610175d747e6451bb0e3d1c51e67
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-31 21:22:35 +0200

    Fixed imports

--- migasfree/device/models/device.py
+++ migasfree/device/models/device.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import json
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
-from .models import Connection, Model
+from .connection import Connection
+from .model import Model
 
 
+@python_2_unicode_compatible
 class Device(models.Model):
     name = models.CharField(
         verbose_name=_("name"),
@@ -42,8 +61,8 @@ class Device(models.Model):
             self.connection.name: json.loads(self.data),
         }
 
-    def __unicode__(self):
-        return u'%s__%s__%s' % (
+    def __str__(self):
+        return '%s__%s__%s' % (
             self.name,
             self.model.name,
             self.connection.name
