a8bd77cd05592218ae0a6996f617d93a11a193ea
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-31 21:22:19 +0200

    Fixed imports

--- migasfree/device/models/connection.py
+++ migasfree/device/models/connection.py
@@ -1,21 +1,39 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
-from .models import Type
+from .type import Type
 
 
+@python_2_unicode_compatible
 class Connection(models.Model):
     name = models.CharField(
-        _("name"),
+        verbose_name=_("name"),
         max_length=50,
         null=True,
         blank=True
     )
 
     fields = models.CharField(
-        _("fields"),
+        verbose_name=_("fields"),
         max_length=100,
         null=True,
         blank=True,
@@ -27,14 +45,11 @@ class Connection(models.Model):
         verbose_name=_("device type")
     )
 
-    def __unicode__(self):
-        return u'(%s) %s' % (self.device_type.name, self.name)
+    def __str__(self):
+        return '(%s) %s' % (self.device_type.name, self.name)
 
     class Meta:
         app_label = 'device'
         verbose_name = _("Device (Connection)")
         verbose_name_plural = _("Device (Connections)")
         unique_together = (("device_type", "name"),)
-        permissions = (
-            ("can_save_device_connection", "Can save Device Connection"),
-        )
