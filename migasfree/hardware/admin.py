69589086dab1cbd25fc51e56e8f571cad3697f87
Jose Antonio Chavarría <jachavar@gmail.com>
2015-03-31 14:31:40 +0200

    Added hardware models to admin site

--- migasfree/hardware/admin.py
+++ migasfree/hardware/admin.py
@@ -1,3 +1,26 @@
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Capability, Configuration, LogicalName, Node
+
+admin.site.register(Capability)
+admin.site.register(Configuration)
+admin.site.register(LogicalName)
+admin.site.register(Node)
