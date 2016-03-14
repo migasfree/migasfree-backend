48d00708fde15eaeeb3119733d94907d4ecd8295
Jose Antonio Chavarría <jachavar@gmail.com>
2016-03-14 19:09:57 +0100

    Changed default DJANGO_SETTINGS_MODULE to production settings

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migasfree.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "migasfree.settings.production"
+    )
 
     from django.core.management import execute_from_command_line
 
