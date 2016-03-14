af0384795f73e08f575e3257d67e0e9e8ddc73e1
Jose Antonio Chavarría <jachavar@gmail.com>
2016-03-14 19:36:28 +0100

    Fixed ScheduleDelay.__str__ method if delay is None

--- migasfree/core/models/schedule_delay.py
+++ migasfree/core/models/schedule_delay.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -49,7 +49,7 @@ class ScheduleDelay(models.Model):
     )
 
     def __str__(self):
-        return '%s (%d)' % (self.schedule.name, self.delay)
+        return '%s (%s)' % (self.schedule.name, str(self.delay))
 
     def attribute_list(self):
         return ', '.join(
