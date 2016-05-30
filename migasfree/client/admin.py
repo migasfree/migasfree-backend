6e57ebf0fb90144045b56a9450d11da818e5b100
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 12:15:55 +0200

    Removed unused import

--- migasfree/client/admin.py
+++ migasfree/client/admin.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.contrib.admin import SimpleListFilter
