ef0390ec3d459aa76ce6eaa8ace3f61a67f083b1
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 12:09:50 +0200

    Removed unused import

--- migasfree/core/models/project.py
+++ migasfree/core/models/project.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,7 +26,6 @@ from django.utils.text import slugify
 from django.conf import settings
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
-from django.core.exceptions import ValidationError
 
 from . import Platform
 
