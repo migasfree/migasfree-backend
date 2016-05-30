e43d464830a1bb2cf969f5012943181919e0ab66
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 12:09:10 +0200

    Fixed unreachable code

--- migasfree/core/fields.py
+++ migasfree/core/fields.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -59,7 +59,6 @@ class MultiFileField(forms.FileField):
                     'num_files': num_files
                 }
             )
-            return
         elif self.max_num and  num_files > self.max_num:
             raise ValidationError(
                 self.error_messages['max_num'] % {
