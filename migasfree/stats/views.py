265dc9fdeb95ceb5b31597f24d8e0f27e90831a4
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 12:34:08 +0200

    Removed unused import

--- migasfree/stats/views.py
+++ migasfree/stats/views.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
 
 import time
 
-from datetime import date, timedelta, datetime
+from datetime import timedelta, datetime
 from dateutil import relativedelta
 
 from django.shortcuts import get_object_or_404
