07de759541084634a6618ed39ec3c3b21b57a42e
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 13:24:15 +0200

    Fixed PackageManager.create method

--- migasfree/client/models/package.py
+++ migasfree/client/models/package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ from migasfree.core.models import Project
 class PackageManager(models.Manager):
     def create(self, fullname, name, version, architecture, project):
         package = Package(
-            fullname=package,
+            fullname=fullname,
             name=name,
             version=version,
             architecture=architecture,
