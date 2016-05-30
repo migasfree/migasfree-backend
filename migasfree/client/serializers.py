4f842582c3607d32931c0621cb047b067eb7b953
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 12:16:32 +0200

    Removed unused import

--- migasfree/client/serializers.py
+++ migasfree/client/serializers.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 from . import models
