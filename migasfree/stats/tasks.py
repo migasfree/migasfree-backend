bb160641c8485f8e2f8e050c98941efbc0cd636d
Jose Antonio Chavarría <jachavar@gmail.com>
2016-05-30 12:15:09 +0200

    Removed unused import

--- migasfree/stats/tasks.py
+++ migasfree/stats/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,7 +26,6 @@ from django.db.models import Q
 from django.conf import settings
 from django.utils.translation import ugettext
 from celery import shared_task
-from celery.exceptions import Ignore
 from django_redis import get_redis_connection
 from rest_framework.reverse import reverse
 
