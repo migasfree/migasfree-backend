5e98d8f84e434ba64b2603a43f76578a30417890
Jose Antonio Chavarría <jachavar@gmail.com>
2016-07-05 11:57:21 +0200

    Encapsulation improvements

--- migasfree/core/tasks.py
+++ migasfree/core/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,10 +24,9 @@ import shutil
 from celery import shared_task
 from celery.exceptions import Ignore
 from django_redis import get_redis_connection
-from django.conf import settings
 from importlib import import_module
 
-from .models import Deployment, Package
+from .models import Deployment, Package, Project, Store
 
 
 @shared_task(queue='repository')
@@ -42,16 +41,7 @@ def remove_repository_metadata(deployment_id, old_slug=''):
     else:
         slug = deploy.slug
 
-    mod = import_module('migasfree.core.pms.%s' % deploy.project.pms)
-    pms = getattr(mod, deploy.project.pms.capitalize())()
-
-    destination = os.path.join(
-        settings.MIGASFREE_PUBLIC_DIR,
-        deploy.project.slug,
-        pms.relative_path,
-        slug
-    )
-    shutil.rmtree(destination, ignore_errors=True)
+    shutil.rmtree(deploy.path(slug), ignore_errors=True)
 
 
 @shared_task(queue='repository')
@@ -70,24 +60,11 @@ def create_repository_metadata(deployment_id):
     )
     con.sadd('migasfree:watch:repos', deploy.id)
 
-    mod = import_module('migasfree.core.pms.%s' % deploy.project.pms)
-    pms = getattr(mod, deploy.project.pms.capitalize())()
-
-    tmp_path = os.path.join(
-        settings.MIGASFREE_PUBLIC_DIR,
-        deploy.project.slug,
-        'tmp'
-    )
-    stores_path = os.path.join(
-        settings.MIGASFREE_PUBLIC_DIR,
-        deploy.project.slug,
-        'stores'
-    )
+    tmp_path = deploy.path('tmp')
+    stores_path = Store.path(deploy.project.slug, '')[:-1]  # remove trailing slash
     slug_tmp_path = os.path.join(
-        settings.MIGASFREE_PUBLIC_DIR,
-        deploy.project.slug,
-        'tmp',
-        pms.relative_path
+        tmp_path,
+        deploy.pms_path()
     )
 
     if slug_tmp_path.endswith('/'):
@@ -97,14 +74,14 @@ def create_repository_metadata(deployment_id):
     pkg_tmp_path = os.path.join(
         slug_tmp_path,
         deploy.slug,
-        'PKGS'
+        'PKGS'  # FIXME hardcoded path!!!
     )
     if not os.path.exists(pkg_tmp_path):
         os.makedirs(pkg_tmp_path)
 
     for pkg_id in deploy.available_packages.values_list('id', flat=True):
         pkg = Package.objects.get(id=pkg_id)
-        dst = os.path.join(slug_tmp_path, deploy.slug, 'PKGS', pkg.name)
+        dst = os.path.join(pkg_tmp_path, pkg.name)
         if not os.path.lexists(dst):
             os.symlink(
                 os.path.join(stores_path, pkg.store.slug, pkg.name),
@@ -117,15 +94,10 @@ def create_repository_metadata(deployment_id):
 
     source = os.path.join(
         tmp_path,
-        pms.relative_path,
+        deploy.pms_path(),
         deploy.slug
     )
-    target = os.path.join(
-        settings.MIGASFREE_PUBLIC_DIR,
-        deploy.project.slug,
-        pms.relative_path,
-        deploy.slug
-    )
+    target = deploy.path()
     shutil.rmtree(target, ignore_errors=True)
     shutil.copytree(source, target, symlinks=True)
     shutil.rmtree(tmp_path)
@@ -133,4 +105,4 @@ def create_repository_metadata(deployment_id):
     con.hdel('migasfree:repos:%d' % deploy.id, '*')
     con.srem('migasfree:watch:repos', deploy.id)
 
-    return (ret, output if ret == 0 else error)
+    return ret, output if ret == 0 else error
