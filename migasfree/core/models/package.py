ded4d1cb5eee1de2a5b4a38a6e2c9895b43785bf
Jose Antonio Chavarría <jachavar@gmail.com>
2017-02-22 17:42:09 +0100

    Added project validation in clean method

--- migasfree/core/models/package.py
+++ migasfree/core/models/package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2017 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2017 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -75,6 +75,9 @@ class Package(models.Model):
 
     def clean(self):
         super(Package, self).clean()
+
+        if not hasattr(self, 'project'):
+            return False
 
         if self.store.project.id != self.project.id:
             raise ValidationError(_('Store must belong to the project'))
