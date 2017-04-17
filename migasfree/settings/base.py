e8d69effff2cb692818510338f96193da37aa756
Jose Antonio Chavarría <jachavar@gmail.com>
2017-04-17 09:25:22 +0200

    Added parameters in REST_FRAMEWORK setting

--- migasfree/settings/base.py
+++ migasfree/settings/base.py
@@ -119,6 +119,9 @@ INSTALLED_APPS = (
 )
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissions',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
@@ -126,6 +129,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework_filters.backends.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
     'DATETIME_FORMAT': '%c',
 }
 
