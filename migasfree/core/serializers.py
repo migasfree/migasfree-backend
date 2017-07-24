719808f4cb18bcee00ccc6edd38ffdf9e695165d
Jose Antonio Chavarría <jachavar@gmail.com>
2017-07-24 07:57:29 +0200

    Commented unused imports

--- migasfree/core/serializers.py
+++ migasfree/core/serializers.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from django.contrib.auth.models import User, Group
+# from django.contrib.auth.models import User, Group
 from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
