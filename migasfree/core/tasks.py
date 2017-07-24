26a6cfa0a54c9fc87d255beb398b5ede0af108ad
Jose Antonio Chavarría <jachavar@gmail.com>
2017-07-24 07:56:41 +0200

    Removed default argument value in get_redis_connection

--- migasfree/core/tasks.py
+++ migasfree/core/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2017 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2017 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -51,7 +51,7 @@ def create_repository_metadata(deployment_id):
     except:
         raise Ignore()
 
-    con = get_redis_connection('default')
+    con = get_redis_connection()
     con.hmset(
         'migasfree:repos:%d' % deploy.id, {
             'name': deploy.name,
