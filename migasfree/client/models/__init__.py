087757a9aa015e9d8b9edbe13bbf6b741f3f9173
Jose Antonio Chavarría <jachavar@gmail.com>
2017-07-24 08:25:57 +0200

    Changed Package by PackageHistory model

--- migasfree/client/models/__init__.py
+++ migasfree/client/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2017 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2017 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 from .computer import Computer
 from .status_log import StatusLog
 
-from .package import Package
+from .package_history import PackageHistory
 from .error import Error
 
 from .fault_definition import FaultDefinition
