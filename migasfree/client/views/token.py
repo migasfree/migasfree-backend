7b3651110e254af5f17adc98662cfe7ee3aa439d
Jose Antonio Chavarría <jachavar@gmail.com>
2017-12-07 12:42:28 +0100

    Removed unused import

--- migasfree/client/views/token.py
+++ migasfree/client/views/token.py
@@ -28,8 +28,6 @@ from rest_framework.response import Response
 # from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework_filters import backends
 
-from migasfree.core.serializers import PackageSerializer
-
 from .. import models, serializers
 from ..filters import (
     PackageHistoryFilter, ErrorFilter, NotificationFilter,
