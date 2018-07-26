8fedfc48edbe44e2fc89817fca9878609d25ca23
Jose Antonio Chavarría <jachavar@gmail.com>
2018-07-26 13:23:54 +0200

    Fixed exception type in update_software_inventory task

--- migasfree/client/tasks.py
+++ migasfree/client/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2017 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2017 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@ from __future__ import absolute_import
 from datetime import datetime
 
 from django.db import connection
+from django.core.exceptions import ObjectDoesNotExist
 from celery import shared_task
 from celery.exceptions import Ignore
 
@@ -32,7 +33,7 @@ from .models import Computer
 def update_software_inventory(computer_id, inventory):
     try:
         computer = Computer.objects.get(pk=computer_id)
-    except:
+    except ObjectDoesNotExist:
         raise Ignore()
 
     if inventory and isinstance(inventory, list):
@@ -67,7 +68,7 @@ def update_software_inventory_raw(pkgs, computer_id, project_id):
     ) AS P ON tmp.name=P.name AND tmp.version=P.version AND tmp.architecture=P.architecture
     WHERE tmp.name IS NULL;
     """ % {
-        'pkgs': str(pkgs)[1:-1],  # remove '[' and ']' characters
+        'pkgs': str(pkgs)[1:-1],
         'cid': computer_id,
         'project': project_id
     }
@@ -75,11 +76,15 @@ def update_software_inventory_raw(pkgs, computer_id, project_id):
     to_remove = [x[0] for x in cursor.fetchall()]
     if to_remove:
         sql = """
-        UPDATE client_packagehistory SET uninstall_date='%s'
-        WHERE client_packagehistory.package_id IN (%s)
+        UPDATE client_packagehistory SET uninstall_date='%(date)s'
+        WHERE client_packagehistory.package_id IN (%(pkgs)s)
             AND uninstall_date IS NULL
-            AND computer_id=%s;
-        """ % (str(now), str(to_remove)[1:-1], computer_id)
+            AND computer_id=%(cid)s;
+        """ % {
+            'date': str(now),
+            'pkgs': str(to_remove)[1:-1],
+            'cid': computer_id
+        }
         cursor.execute(sql)
 
     # INSERT PKG
@@ -95,7 +100,7 @@ def update_software_inventory_raw(pkgs, computer_id, project_id):
         AND tmp.architecture=P.architecture AND tmp.fullname=P.fullname
     WHERE P.name IS NULL;
     """ % {
-        'pkgs': str(pkgs)[1:-1],  # remove '[' and ']' characters
+        'pkgs': str(pkgs)[1:-1],
         'project': project_id
     }
     cursor.execute(sql)
@@ -112,7 +117,7 @@ def update_software_inventory_raw(pkgs, computer_id, project_id):
         sql = """
         INSERT INTO core_package(name, version, architecture, fullname, project_id)
         VALUES %s;
-        """ % str(to_add)[1:-1]  # remove '[' and ']' characters
+        """ % str(to_add)[1:-1]
         cursor.execute(sql)
 
     # INSERT M2M
@@ -131,7 +136,7 @@ def update_software_inventory_raw(pkgs, computer_id, project_id):
     ) AS P ON tmp.name=P.name AND tmp.version=P.version
     WHERE tmp.name IS NOT NULL;
     """ % {
-        'pkgs': str(pkgs)[1:-1],  # remove '[' and ']' characters
+        'pkgs': str(pkgs)[1:-1],
         'cid': computer_id,
         'project': project_id
     }
@@ -141,5 +146,5 @@ def update_software_inventory_raw(pkgs, computer_id, project_id):
         sql = """
         INSERT INTO client_packagehistory(computer_id, package_id, install_date)
         VALUES %s;
-        """ % str(to_m2m_history)[1:-1]  # remove '[' and ']' characters
+        """ % str(to_m2m_history)[1:-1]
         cursor.execute(sql)
