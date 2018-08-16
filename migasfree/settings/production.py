ed34d95f0053feb1d7435561e27132e789b7e811
Jose Antonio Chavarría <jachavar@gmail.com>
2018-08-16 12:19:12 +0200

    Python 3 compatibility code

--- migasfree/settings/production.py
+++ migasfree/settings/production.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2017 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2017 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,6 +15,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from past.builtins import execfile
 
 from .migasfree import *
 from .base import *
