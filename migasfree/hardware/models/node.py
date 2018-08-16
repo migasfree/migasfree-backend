ecbcb0671227e2a7f744ece6171625ef38073efb
Jose Antonio Chavarría <jachavar@gmail.com>
2018-08-16 12:19:05 +0200

    Python 3 compatibility code

--- migasfree/hardware/models/node.py
+++ migasfree/hardware/models/node.py
@@ -18,6 +18,8 @@
 
 import re
 
+from past.builtins import basestring
+
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
