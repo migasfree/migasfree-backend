3e29e3993752e4c18869d7a66bbf5d75b9e1d758
Jose Antonio Chavarría <jachavar@gmail.com>
2019-11-29 13:36:47 +0100

    Fixed warning open file but not closed

--- migasfree/settings/functions.py
+++ migasfree/settings/functions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2019 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2019 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,7 +27,8 @@ def secret_key(path):
 
     _file = os.path.join(path, 'secret_key.txt')
     if os.path.exists(_file):
-        key = open(_file).read().strip()
+        with open(_file) as f:
+            key = f.read().strip()
     else:
         key = ''.join([random.SystemRandom().choice("%s%s%s" % (
             string.ascii_letters,
