35136dc695a708637c67869e87d2e51de5d465bc
Jose Antonio Chavarría <jachavar@gmail.com>
2019-12-02 12:59:15 +0100

    Changed filters import (DRF upgrade)

--- migasfree/device/filters.py
+++ migasfree/device/filters.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2016-2017 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2016-2017 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2016-2019 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2016-2019 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import rest_framework_filters as filters
+from django_filters import rest_framework as filters
 
 from .models import Device, Driver
 
