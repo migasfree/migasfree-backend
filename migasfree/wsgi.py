bb17e9fc6b7c41f74fded52a4362f77cfac65005
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-12 12:25:55 +0100

    Changed IO priority to idle (migasfree/migasfree#299)

--- migasfree/wsgi.py
+++ migasfree/wsgi.py
@@ -4,12 +4,16 @@ WSGI config for migasfree project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import psutil
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migasfree.settings.production")
 application = get_wsgi_application()
+
+p = psutil.Process(os.getpid())
+p.ionice(psutil.IOPRIO_CLASS_IDLE)
