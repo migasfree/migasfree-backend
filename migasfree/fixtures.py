bbc86e74ed2214bdafc0e77f48d1e8b1079954cf
Jose Antonio Chavarría <jachavar@gmail.com>
2020-02-22 12:38:52 +0100

    Removed Python2 compatibility code

--- migasfree/fixtures.py
+++ migasfree/fixtures.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -24,10 +24,7 @@ import tempfile
 
 import django.core.management
 
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
+from io import StringIO
 
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth.management import create_permissions
