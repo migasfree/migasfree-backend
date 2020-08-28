4bfcaa882256a97a1dcde8c100a226cd0004f1fe
jact <jachavar@gmail.com>
2020-08-28 12:45:50 +0200

    RemovedInDjango40Warning: django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy()

--- migasfree/app_catalog/models.py
+++ migasfree/app_catalog/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2017-2019 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2017-2019 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2017-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2017-2020 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from markdownx.models import MarkdownxField
 
