2ff47e2907937fe394186edbf3a34a91ffb3381b
jact <jachavar@gmail.com>
2020-08-28 12:44:39 +0200

    RemovedInDjango40Warning: django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy()

--- migasfree/core/models/scope.py
+++ migasfree/core/models/scope.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2018-2019 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2018-2019 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2018-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2018-2020 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.template.defaultfilters import slugify
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .attribute import Attribute
 from .domain import Domain
