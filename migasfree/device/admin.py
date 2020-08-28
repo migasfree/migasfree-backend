e1657c31dc60b2d89b81c244d3698fabb10ebff0
jact <jachavar@gmail.com>
2020-08-28 12:48:47 +0200

    RemovedInDjango40Warning: django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy()

--- migasfree/device/admin.py
+++ migasfree/device/admin.py
@@ -19,7 +19,7 @@
 from django import forms
 from django.contrib import admin
 from django.db.models import Prefetch
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from migasfree.core.models import Attribute
 from .models import (
