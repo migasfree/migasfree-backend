26279ebc3efc750dcc5d027374a57ca0ba86ae85
jact <jachavar@gmail.com>
2020-08-28 12:45:27 +0200

    RemovedInDjango40Warning: django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy()

--- migasfree/client/models/package_history.py
+++ migasfree/client/models/package_history.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2017-2019 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2017-2019 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2017-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2017-2020 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from ...core.models import Package
 from .computer import Computer
