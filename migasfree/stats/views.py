6d9cf5e11d0a19cc79d4ba0c6f3274c7d5cafc1c
jact <jachavar@gmail.com>
2020-10-02 15:23:31 +0200

    Added several stats

--- migasfree/stats/views.py
+++ migasfree/stats/views.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,19 +18,23 @@
 
 import time
 
-from datetime import timedelta, datetime
-from dateutil import relativedelta
+from datetime import timedelta, datetime, date
+from dateutil.relativedelta import relativedelta
 
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.translation import ugettext as _
 from django_redis import get_redis_connection
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
 
-from migasfree.core.models import Project, Deployment
-from migasfree.client.models import Computer
+from ..core.models import Project, Deployment
+from ..client.models import Computer
+from ..utils import replace_keys
 
-from . import validators
+from . import validators, MONTHLY_RANGE
 
 
 def month_year_iter(start_month, start_year, end_month, end_year):
@@ -42,10 +46,108 @@ def month_year_iter(start_month, start_year, end_month, end_year):
         yield y, m + 1
 
 
+def first_day_month(date_):
+    return date(date_.year, date_.month, 1)
+
+
+def month_interval():
+    delta = relativedelta(months=+1)
+    end_date = date.today() + delta
+    begin_date = end_date - relativedelta(months=+MONTHLY_RANGE)
+
+    return first_day_month(begin_date), end_date
+
+
 def daterange(start_date, end_date):
     # http://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
+
+
+def event_by_month(data, begin_date, end_date, model, field='project_id'):
+    labels = {}
+    new_data = {}
+    chart_data = {}
+    url = reverse('admin:client_{}_changelist'.format(model))
+
+    if field == 'project_id':
+        projects = Project.objects.only('id', 'name')
+        for project in projects:
+            new_data[project.id] = []
+            labels[project.id] = project.name
+    elif field == 'status':
+        for status in Computer.STATUS_CHOICES:
+            new_data[status[0]] = []
+            labels[status[0]] = _(status[1])
+    elif field == 'checked':
+        new_data[True] = []
+        new_data[False] = []
+        labels[True] = _('Checked')
+        labels[False] = _('Unchecked')
+
+    # shuffle data series
+    x_axe = []
+    for monthly in month_year_iter(
+        begin_date.month, begin_date.year,
+        end_date.month, end_date.year
+    ):
+        start_date = date(monthly[0], monthly[1], 1)
+        final_date = start_date + relativedelta(months=+1)
+        querystring = {
+            'created_at__gte': start_date.strftime('%Y-%m-%d'),
+            'created_at__lt': final_date.strftime('%Y-%m-%d')
+        }
+
+        key = '%d-%02d' % (monthly[0], monthly[1])
+        x_axe.append(key)
+        value = list(filter(lambda item: item['year'] == monthly[0] and item['month'] == monthly[1], data))
+        if field == 'project_id':
+            for project in projects:
+                if value:
+                    count = list(filter(lambda item: item['project_id'] == project.id, value))
+                    querystring['project__id__exact'] = project.id
+                    new_data[project.id].append({
+                        'value': count[0]['count'] if count else 0,
+                        'url': '{}?{}'.format(url, urlencode(querystring))
+                    })
+                else:
+                    new_data[project.id].append({
+                        'value': 0,
+                        'url': '#'
+                    })
+        elif field == 'status':
+            for status in Computer.STATUS_CHOICES:
+                if value:
+                    count = list(filter(lambda item: item['status'] == status[0], value))
+                    querystring['status__in'] = status[0]
+                    new_data[status[0]].append({
+                        'value': count[0]['count'] if count else 0,
+                        'url': '{}?{}'.format(url, urlencode(querystring))
+                    })
+                else:
+                    new_data[status[0]].append({
+                        'value': 0,
+                        'url': '#'
+                    })
+        elif field == 'checked':
+            for val in [True, False]:
+                if value:
+                    count = list(filter(lambda item: item['checked'] == val, value))
+                    querystring['checked__exact'] = 1 if val else 0
+                    new_data[val].append({
+                        'value': count[0]['count'] if count else 0,
+                        'url': '{}?{}'.format(url, urlencode(querystring))
+                    })
+                else:
+                    new_data[val].append({
+                        'value': 0,
+                        'url': '#'
+                    })
+
+    for item in new_data:
+        chart_data[labels[item]] = new_data[item]
+
+    return {'x_labels': x_axe, 'data': chart_data}
 
 
 @permission_classes((permissions.IsAuthenticated,))
@@ -197,35 +299,29 @@ class SyncStatsViewSet(viewsets.ViewSet):
 class ComputerStatsViewSet(viewsets.ViewSet):
     @action(methods=['get'], detail=False)
     def projects(self, request, format=None):
-        data = list(Computer.group_by_project())
-        aliases = {
-            'project__name': 'name',
-            'project__id': 'project_id',
-            'count': 'value'
-        }
-
-        for i, item in enumerate(data):
-            data[i] = dict((aliases[key], value) for (key, value) in item.items())
-
         return Response(
-            data,
+            replace_keys(
+                list(Computer.group_by_project()),
+                {
+                    'project__name': 'name',
+                    'project__id': 'project_id',
+                    'count': 'value'
+                }
+            ),
             status=status.HTTP_200_OK
         )
 
     @action(methods=['get'], detail=False)
     def platforms(self, request, format=None):
-        data = list(Computer.group_by_platform())
-        aliases = {
-            'project__platform__name': 'name',
-            'project__platform__id': 'platform_id',
-            'count': 'value'
-        }
-
-        for i, item in enumerate(data):
-            data[i] = dict((aliases[key], value) for (key, value) in item.items())
-
         return Response(
-            data,
+            replace_keys(
+                list(Computer.group_by_platform()),
+                {
+                    'project__platform__name': 'name',
+                    'project__platform__id': 'platform_id',
+                    'count': 'value'
+                }
+            ),
             status=status.HTTP_200_OK
         )
 
@@ -236,6 +332,45 @@ class ComputerStatsViewSet(viewsets.ViewSet):
 
         return Response(
             Computer.count_by_attributes(attributes, project_id),
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=['get'], detail=False, url_path='new/month')
+    def new_computers_by_month(self, request, format=None):
+        begin_date, end_date = month_interval()
+
+        data = event_by_month(
+            Computer.stacked_by_month(request.user.userprofile, begin_date),
+            begin_date,
+            end_date,
+            'computer'
+        )
+        return Response(
+            data,
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=['get'], detail=False, url_path='productive/platform')
+    def productive_computers_by_platform(self, request, format=None):
+        data = Computer.productive_computers_by_platform(request.user.userprofile)
+        inner_aliases = {
+            'project__platform__id': 'platform_id',
+            'project__platform__name': 'name',
+            'count': 'value'
+        }
+        outer_aliases = {
+            'project__name': 'name',
+            'project__id': 'project_id',
+            'project__platform__id': 'platform_id',
+            'count': 'value'
+        }
+
+        return Response(
+            {
+                'total': data['total'],
+                'inner': replace_keys(data['inner'], inner_aliases),
+                'outer': replace_keys(data['outer'], outer_aliases)
+            },
             status=status.HTTP_200_OK
         )
 
