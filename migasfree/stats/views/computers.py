a51b9017c93a94eb0d8a9130684612f7f96c72d9
jact <jachavar@gmail.com>
2020-10-05 12:48:57 +0200

    Added several methods in ViewSet

--- migasfree/stats/views/computers.py
+++ migasfree/stats/views/computers.py
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from django.db.models import Count
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.translation import gettext as _
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
@@ -56,6 +60,186 @@ class ComputerStatsViewSet(viewsets.ViewSet):
             status=status.HTTP_200_OK
         )
 
+    @action(methods=['get'], detail=False, url_path='machine')
+    def by_machine(self, request, format=None):
+        total = Computer.objects.scope(request.user.userprofile).count()
+        link = '{}?_REPLACE_'.format(
+            reverse('admin:client_computer_changelist')
+        )
+
+        data = {
+            'inner': [],
+            'outer': [],
+        }
+
+        count_subscribed = Computer.subscribed.scope(request.user.userprofile).count()
+        count_subscribed_virtual = Computer.subscribed.scope(request.user.userprofile).filter(machine='V').count()
+        count_subscribed_physical = Computer.subscribed.scope(request.user.userprofile).filter(machine='P').count()
+        count_unsubscribed = Computer.unsubscribed.scope(request.user.userprofile).count()
+        count_unsubscribed_virtual = Computer.unsubscribed.scope(request.user.userprofile).filter(machine='V').count()
+        count_unsubscribed_physical = Computer.unsubscribed.scope(request.user.userprofile).filter(machine='P').count()
+
+        if count_subscribed:
+            if count_subscribed_virtual:
+                data['outer'].append(
+                    {
+                        'name': _('Virtual'),
+                        'value': count_subscribed_virtual,
+                        'url': link.replace(
+                            '_REPLACE_',
+                            'status__in=intended,reserved,unknown,available,in repair&machine__exact=V'
+                        )
+                    }
+                )
+
+            if count_subscribed_physical:
+                data['outer'].append(
+                    {
+                        'name': _('Physical'),
+                        'value': count_subscribed_physical,
+                        'url': link.replace(
+                            '_REPLACE_',
+                            'status__in=intended,reserved,unknown,available,in repair&machine__exact=P'
+                        )
+                    }
+                )
+
+            data['inner'].append(
+                {
+                    'name': _('Subscribed'),
+                    'value': count_subscribed,
+                    'url': link.replace(
+                        '_REPLACE_',
+                        'status__in=intended,reserved,unknown,available,in repair'
+                    ),
+                },
+            )
+
+        if count_unsubscribed:
+            if count_unsubscribed_virtual:
+                data['outer'].append(
+                    {
+                        'name': _('Virtual'),
+                        'value': count_unsubscribed_virtual,
+                        'url': link.replace(
+                            '_REPLACE_',
+                            'status__in=unsubscribed&machine__exact=V'
+                        )
+                    }
+                )
+
+            if count_unsubscribed_physical:
+                data['outer'].append(
+                    {
+                        'name': _('Physical'),
+                        'value': count_unsubscribed_physical,
+                        'url': link.replace(
+                            '_REPLACE_',
+                            'status__in=unsubscribed&machine__exact=P'
+                        )
+                    }
+                )
+
+            data['inner'].append(
+                {
+                    'name': _('Unsubscribed'),
+                    'value': count_unsubscribed,
+                    'url': link.replace(
+                        '_REPLACE_',
+                        'status__in=unsubscribed'
+                    ),
+                }
+            )
+
+        return Response(
+            {
+                'title': _('Computers / Machine'),
+                'total': total,
+                'inner': data['inner'],
+                'outer': data['outer'],
+                'url': link.replace('?_REPLACE_', ''),
+            },
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=['get'], detail=False, url_path='status')
+    def by_status(self, request, format=None):
+        # TODO response format (inner, outer)
+        total = Computer.objects.scope(request.user.userprofile).exclude(status='unsubscribed').count()
+        link = '{}?_REPLACE_'.format(
+            reverse('admin:client_computer_changelist')
+        )
+
+        values = dict()
+        for item in Computer.objects.scope(
+            request.user.userprofile
+        ).exclude(
+            status='unsubscribed'
+        ).values(
+            'status'
+        ).annotate(
+            count=Count('id')
+        ).order_by('status', '-count'):
+            status_name = _(dict(Computer.STATUS_CHOICES)[item.get('status')])
+            values[item.get('status')] = {
+                'name': status_name,
+                'value': item.get('count'),
+                'url': link.replace(
+                    '_REPLACE_',
+                    'status__in={}'.format(item.get('status'))
+                ),
+            }
+
+        count_productive = values.get('intended', {}).get('value', 0) \
+            + values.get('reserved', {}).get('value', 0) \
+            + values.get('unknown', {}).get('value', 0)
+        data_productive = []
+        if 'intended' in values:
+            data_productive.append(values['intended'])
+        if 'reserved' in values:
+            data_productive.append(values['reserved'])
+        if 'unknown' in values:
+            data_productive.append(values['unknown'])
+
+        count_unproductive = values.get('available', {}).get('value', 0) \
+            + values.get('in repair', {}).get('value', 0)
+        data_unproductive = []
+        if 'available' in values:
+            data_unproductive.append(values['available'])
+        if 'in repair' in values:
+            data_unproductive.append(values['in repair'])
+
+        data = [
+            {
+                'name': _('Productive'),
+                'value': count_productive,
+                'url': link.replace(
+                    '_REPLACE_',
+                    'status__in=intended,reserved,unknown'
+                ),
+                'data': data_productive,
+            },
+            {
+                'name': _('Unproductive'),
+                'value': count_unproductive,
+                'url': link.replace(
+                    '_REPLACE_',
+                    'status__in=available,in repair'
+                ),
+                'data': data_unproductive,
+            },
+        ]
+
+        return Response(
+            {
+                'title': _('Subscribed Computers / Status'),
+                'total': total,
+                'data': data,
+                'url': link.replace('_REPLACE_', 'status__in=intended,reserved,unknown,available,in repair'),
+            },
+            status=status.HTTP_200_OK
+        )
+
     @action(methods=['get'], detail=False, url_path='attributes/count')
     def attributes_count(self, request, format=None):
         attributes = request.query_params.getlist('attributes')
@@ -67,7 +251,7 @@ class ComputerStatsViewSet(viewsets.ViewSet):
         )
 
     @action(methods=['get'], detail=False, url_path='new/month')
-    def new_computers_by_month(self, request, format=None):
+    def new_by_month(self, request, format=None):
         begin_date, end_date = month_interval()
 
         data = event_by_month(
@@ -82,7 +266,7 @@ class ComputerStatsViewSet(viewsets.ViewSet):
         )
 
     @action(methods=['get'], detail=False, url_path='productive/platform')
-    def productive_computers_by_platform(self, request, format=None):
+    def productive_by_platform(self, request, format=None):
         data = Computer.productive_computers_by_platform(request.user.userprofile)
         inner_aliases = {
             'project__platform__id': 'platform_id',
@@ -101,6 +285,36 @@ class ComputerStatsViewSet(viewsets.ViewSet):
                 'total': data['total'],
                 'inner': replace_keys(data['inner'], inner_aliases),
                 'outer': replace_keys(data['outer'], outer_aliases)
+            },
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=['get'], detail=False, url_path='entry/year')
+    def entry_year(self, request, format=None):
+        url = '{}?machine__exact=P&_REPLACE_'.format(
+            reverse('admin:client_computer_changelist')
+        )
+        results = Computer.entry_year(request.user.userprofile)
+        data = [x['count'] for x in results]
+        labels = [x['year'] for x in results]
+
+        for i, item in enumerate(labels):
+            querystring = {
+                'created_at__gte': '{}-01-01'.format(labels[i]),
+                'created_at__lt': '{}-01-01'.format(labels[i] + 1)
+            }
+            data[i] = {
+                'value': data[i],
+                'url': url.replace(
+                    '_REPLACE_',
+                    urlencode(querystring)
+                )
+            }
+
+        return Response(
+            {
+                'x_labels': labels,
+                'data': {_('Computers'): data}
             },
             status=status.HTTP_200_OK
         )
