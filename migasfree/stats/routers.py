2d705dc8dd06b7d47771d2775817b81851e4a65f
jact <jachavar@gmail.com>
2020-10-05 13:42:51 +0200

    Added ClientAttributeStatsViewSet, ServerAttributeStatsViewSet

--- migasfree/stats/routers.py
+++ migasfree/stats/routers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -30,4 +30,14 @@ router.register(
     r'stats/deployments',
     views.DeploymentStatsViewSet,
     basename='stats-deployments'
+)
+router.register(
+    r'stats/features',
+    views.ClientAttributeStatsViewSet,
+    basename='stats-features'
+)
+router.register(
+    r'stats/tags',
+    views.ServerAttributeStatsViewSet,
+    basename='stats-tags'
 )
