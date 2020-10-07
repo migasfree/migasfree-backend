624da609a3c72311fd95b0a7251c752171ccf14a
jact <jachavar@gmail.com>
2020-10-07 13:03:37 +0200

    Added more stats

--- migasfree/stats/views/__init__.py
+++ migasfree/stats/views/__init__.py
@@ -2,6 +2,7 @@ HOURLY_RANGE = 3  # days
 DAILY_RANGE = 35  # days
 MONTHLY_RANGE = 18  # months
 
+from .applications import ApplicationStatsViewSet
 from .attributes import ClientAttributeStatsViewSet, ServerAttributeStatsViewSet
 from .computers import ComputerStatsViewSet
 from .deployments import DeploymentStatsViewSet
@@ -10,6 +11,7 @@ from .errors import ErrorStatsViewSet
 from .faults import FaultStatsViewSet
 from .migrations import MigrationStatsViewSet
 from .notifications import NotificationStatsViewSet
+from .packages import PackageStatsViewSet
 from .status_logs import StatusLogStatsViewSet
 from .stores import StoreStatsViewSet
 from .syncs import SyncStatsViewSet
