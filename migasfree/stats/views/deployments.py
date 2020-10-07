fa7669b879bc2f4ab7a28a68e8b105837f880a66
jact <jachavar@gmail.com>
2020-10-07 13:03:10 +0200

    Added provided_computers_by_delay method

--- migasfree/stats/views/deployments.py
+++ migasfree/stats/views/deployments.py
@@ -17,8 +17,10 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from collections import defaultdict
+from datetime import timedelta, datetime
 
-from django.db.models import Count, Q
+from django.db.models import Q
+from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext as _
 from django_redis import get_redis_connection
@@ -27,7 +29,9 @@ from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
 
-from ...core.models import Deployment, Project
+from ...client.models import Computer
+from ...core.models import Deployment, Project, ScheduleDelay
+from ...utils import time_horizon
 
 
 @permission_classes((permissions.IsAuthenticated,))
@@ -89,6 +93,111 @@ class DeploymentStatsViewSet(viewsets.ViewSet):
 
         return Response(
             response,
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=['get'], detail=True, url_path='computers/delay')
+    def provided_computers_by_delay(self, request, pk=None):
+        deploy = get_object_or_404(Deployment, pk=pk)
+        if not deploy.schedule:
+            return Response(
+                {
+                    'detail': _('This deployment has not schedule'),
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        rolling_date = deploy.start_date
+
+        available_data = []
+        provided_data = []
+        labels = []
+        chart_data = {}
+
+        if deploy.domain:
+            q_in_domain = ~Q(sync_attributes__id__in=deploy.domain.included_attributes.all())
+            q_ex_domain = Q(sync_attributes__id__in=deploy.domain.excluded_attributes.all())
+        else:
+            q_in_domain = Q()
+            q_ex_domain = Q()
+
+        lst_attributes = list(deploy.included_attributes.values_list('id', flat=True))
+        value = Computer.productive.scope(request.user.userprofile).filter(
+            Q(sync_attributes__id__in=lst_attributes) &
+            Q(project__id=deploy.project.id)
+        ).exclude(
+            Q(sync_attributes__id__in=deploy.excluded_attributes.all())
+        ).exclude(
+            q_in_domain
+        ).exclude(
+            q_ex_domain
+        ).values('id').distinct().count()
+
+        date_format = '%Y-%m-%d'
+        now = datetime.now()
+
+        delays = ScheduleDelay.objects.filter(
+            schedule__id=deploy.schedule.id
+        ).order_by('delay')
+        len_delays = len(delays)
+
+        for i, item in enumerate(delays):
+            lst_att_delay = list(item.attributes.values_list('id', flat=True))
+
+            start_horizon = datetime.strptime(
+                str(time_horizon(rolling_date, 0)),
+                date_format
+            )
+            if i < (len_delays - 1):
+                end_horizon = datetime.strptime(
+                    str(time_horizon(rolling_date, delays[i + 1].delay - item.delay)),
+                    date_format
+                )
+            else:
+                end_horizon = datetime.strptime(
+                    str(time_horizon(rolling_date, item.duration)),
+                    date_format
+                )
+
+            duration = 0
+            for real_days in range(0, (end_horizon - start_horizon).days):
+                loop_date = start_horizon + timedelta(days=real_days)
+                weekday = int(loop_date.strftime("%w"))  # [0(Sunday), 6]
+                if weekday not in [0, 6]:
+                    value += Computer.productive.scope(request.user.userprofile).extra(
+                        select={'deployment': 'id'},
+                        where=[
+                            'computer_id %% {} = {}'.format(item.duration, duration)
+                        ]
+                    ).filter(
+                        ~ Q(sync_attributes__id__in=lst_attributes) &
+                        Q(sync_attributes__id__in=lst_att_delay) &
+                        Q(project__id=deploy.project.id)
+                    ).exclude(
+                        Q(sync_attributes__id__in=deploy.excluded_attributes.all())
+                    ).exclude(
+                        q_in_domain
+                    ).exclude(
+                        q_ex_domain
+                    ).values('id').distinct().count()
+                    duration += 1
+
+                labels.append(loop_date.strftime(date_format))
+                provided_data.append(value)
+                if loop_date <= now:
+                    available_data.append(value)
+
+            lst_attributes += lst_att_delay
+            rolling_date = end_horizon.date()
+
+        chart_data[_('Provided')] = provided_data
+        chart_data[_('Available')] = available_data
+
+        return Response(
+            {
+                'data': chart_data,
+                'x_labels': list(labels),
+            },
             status=status.HTTP_200_OK
         )
 
@@ -187,7 +296,7 @@ class DeploymentStatsViewSet(viewsets.ViewSet):
             )
 
         values_not_null = defaultdict(list)
-        for item in Deployment.objects.scope(user).filter(
+        for item in Deployment.objects.scope(request.user.userprofile).filter(
             enabled=False,
         ).values(
             'project__id',
