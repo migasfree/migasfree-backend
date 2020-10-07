a56ab55e1b438547718c6d3d3dd11a91969e0989
jact <jachavar@gmail.com>
2020-10-07 13:03:43 +0200

    Added more stats

--- migasfree/stats/routers.py
+++ migasfree/stats/routers.py
@@ -22,9 +22,15 @@ from . import views
 
 router = routers.DefaultRouter()
 
-router.register(r'stats/syncs', views.SyncStatsViewSet, basename='stats-syncs')
 router.register(
-    r'stats/computers', views.ComputerStatsViewSet, basename='stats-computers'
+    r'stats/applications',
+    views.ApplicationStatsViewSet,
+    basename='stats-applications'
+)
+router.register(
+    r'stats/computers',
+    views.ComputerStatsViewSet,
+    basename='stats-computers'
 )
 router.register(
     r'stats/deployments',
@@ -32,34 +38,9 @@ router.register(
     basename='stats-deployments'
 )
 router.register(
-    r'stats/features',
-    views.ClientAttributeStatsViewSet,
-    basename='stats-features'
-)
-router.register(
-    r'stats/tags',
-    views.ServerAttributeStatsViewSet,
-    basename='stats-tags'
-)
-router.register(
     r'stats/devices',
     views.DeviceStatsViewSet,
     basename='stats-devices'
-)
-router.register(
-    r'stats/migrations',
-    views.MigrationStatsViewSet,
-    basename='stats-migrations'
-)
-router.register(
-    r'stats/status-logs',
-    views.StatusLogStatsViewSet,
-    basename='stats-status-logs'
-)
-router.register(
-    r'stats/notifications',
-    views.NotificationStatsViewSet,
-    basename='stats-notifications'
 )
 router.register(
     r'stats/errors',
@@ -72,7 +53,42 @@ router.register(
     basename='stats-faults'
 )
 router.register(
+    r'stats/features',
+    views.ClientAttributeStatsViewSet,
+    basename='stats-features'
+)
+router.register(
+    r'stats/migrations',
+    views.MigrationStatsViewSet,
+    basename='stats-migrations'
+)
+router.register(
+    r'stats/notifications',
+    views.NotificationStatsViewSet,
+    basename='stats-notifications'
+)
+router.register(
+    r'stats/packages',
+    views.PackageStatsViewSet,
+    basename='stats-packages'
+)
+router.register(
+    r'stats/status-logs',
+    views.StatusLogStatsViewSet,
+    basename='stats-status-logs'
+)
+router.register(
     r'stats/stores',
     views.StoreStatsViewSet,
     basename='stats-stores'
+)
+router.register(
+    r'stats/syncs',
+    views.SyncStatsViewSet,
+    basename='stats-syncs'
+)
+router.register(
+    r'stats/tags',
+    views.ServerAttributeStatsViewSet,
+    basename='stats-tags'
 )
