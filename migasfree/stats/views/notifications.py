74a76b822bb64ba3f6fdb3f770ae913994b03257
jact <jachavar@gmail.com>
2020-11-30 17:17:21 +0100

    Removed unused imports

--- migasfree/stats/views/notifications.py
+++ migasfree/stats/views/notifications.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from django.utils.translation import gettext as _
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
