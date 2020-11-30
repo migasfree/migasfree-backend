017514442c6723025a5fad7f2311141a0733c373
jact <jachavar@gmail.com>
2020-11-30 17:17:48 +0100

    Removed unused imports

--- migasfree/urls.py
+++ migasfree/urls.py
@@ -23,7 +23,6 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from rest_framework import permissions, routers
 from rest_framework.authtoken import views
-from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
