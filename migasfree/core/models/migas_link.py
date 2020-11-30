699088e5a926eaec283dfe3cca3eb9f9abaf1b54
jact <jachavar@gmail.com>
2020-11-30 17:17:06 +0100

    Removed unused imports

--- migasfree/core/models/migas_link.py
+++ migasfree/core/models/migas_link.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2020 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2020 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import json
 
 from django.urls import reverse
 from django.utils.translation import gettext
-from django.utils.html import format_html
-from django.utils.http import urlencode
-from django.template.loader import render_to_string
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 
