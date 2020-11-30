9926eb03136d776f0f6b09d72c56b3cf2a25d641
jact <jachavar@gmail.com>
2020-11-30 17:17:53 +0100

    Removed unused imports

--- migasfree/app_catalog/views.py
+++ migasfree/app_catalog/views.py
@@ -18,7 +18,7 @@
 
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status, filters, permissions
+from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action, permission_classes
 from rest_framework.response import Response
 
