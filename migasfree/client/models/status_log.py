cf5880191c0788a33217bb8b76d836fd2dce8f70
jact <jachavar@gmail.com>
2020-12-21 16:00:46 +0100

    Added by_status static method

--- migasfree/client/models/status_log.py
+++ migasfree/client/models/status_log.py
@@ -16,7 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from functools import reduce
+
 from django.db import models
+from django.db.models.aggregates import Count
 from django.utils.translation import gettext_lazy as _
 
 from .computer import Computer
@@ -52,6 +55,53 @@ class StatusLog(Event):
     )
 
     objects = StatusLogManager()
+
+    @staticmethod
+    def by_status(user):
+        total = StatusLog.objects.scope(user).count()
+
+        data = list(StatusLog.objects.scope(user).values(
+            'status',
+        ).annotate(
+            count=Count('id')
+        ).order_by('status', '-count'))
+
+        subscribed_sum = reduce(
+            lambda x, y: x + y['count'],
+            list(filter(lambda s: s['status'] != 'unsubscribed', data)),
+            0
+        )
+        unsubscribed_sum = list(filter(lambda s: s['status'] == 'unsubscribed', data))
+        unsubscribed_sum = unsubscribed_sum[0]['count'] if len(unsubscribed_sum) > 0 else 0
+
+        inner = []
+        if subscribed_sum:
+            inner.append({
+                'name': _('Subscribed'),
+                'value': subscribed_sum,
+                'status_in': 'intended,reserved,unknown,in repair,available',
+            })
+
+        if unsubscribed_sum:
+            inner.append({
+                'name': _('unsubscribed'),
+                'value': unsubscribed_sum,
+                'status_in': 'unsubscribed',
+            })
+
+        outer = []
+        for item in data:
+            outer.append({
+                'name': _(dict(Computer.STATUS_CHOICES)[item.get('status')]),
+                'value': item.get('count'),
+                'status_in': item.get('status')
+            })
+
+        return {
+            'total': total,
+            'inner': inner,
+            'outer': outer,
+        }
 
     class Meta:
         app_label = 'client'
