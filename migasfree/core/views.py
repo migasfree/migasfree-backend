d4ed1e6909dea38f75b07f52a89b5b47a11a71f8
Jose Antonio Chavarría <jachavar@gmail.com>
2021-01-19 13:49:59 +0100

    Fixed devicelogical_set

--- migasfree/core/views.py
+++ migasfree/core/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -376,7 +376,7 @@ class ClientAttributeViewSet(viewsets.ModelViewSet, MigasViewSet):
         """
 
         attribute = self.get_object()
-        logical_devices = attribute.devicelogical_set.all()
+        logical_devices = attribute.logical_set.all()
 
         if request.method == 'GET':
             serializer = LogicalSerializer(
