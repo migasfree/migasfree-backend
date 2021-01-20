57b1771029c352fdef258e3080d908d281fa5b42
jact <jachavar@gmail.com>
2021-01-20 12:48:33 +0100

    Fixed get_relations method

--- migasfree/core/models/migas_link.py
+++ migasfree/core/models/migas_link.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2020 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2020 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2020-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2020-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -338,7 +338,7 @@ class MigasLink(object):
                                     Attribute.objects.scope(
                                         request.user.userprofile
                                     ).filter(
-                                        devicelogical__device__id=self.id
+                                        logical__device__id=self.id
                                     ).values_list('id', flat=True)
                                 )).replace(" ", "").replace("[", "").replace("]", "")
                             ),
