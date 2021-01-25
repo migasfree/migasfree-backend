ead298d15f6e4c5a904a8f270851a3ff9f6f8e1d
jact <jachavar@gmail.com>
2021-01-25 14:23:02 +0100

    Overrided ApplicationWriteSerializer.to_internal_value method

--- migasfree/app_catalog/serializers.py
+++ migasfree/app_catalog/serializers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2017 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2017 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2017-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2017-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,12 +16,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from django.http import QueryDict
 from rest_framework import serializers
 
-from migasfree.core.serializers import (
+from ..core.serializers import (
     AttributeInfoSerializer, ProjectInfoSerializer
 )
-from migasfree.utils import to_list
+from ..utils import to_list
 from . import models
 
 
@@ -54,7 +55,7 @@ class PackagesByProjectWriteSerializer(serializers.ModelSerializer):
         if 'packages_to_install' in data:
             data['packages_to_install'] = '\n'.join(data.get('packages_to_install', []))
 
-        return super(PackagesByProjectWriteSerializer, self).to_internal_value(data)
+        return super().to_internal_value(data)
 
     class Meta:
         model = models.PackagesByProject
@@ -85,6 +86,35 @@ class ApplicationInfoSerializer(serializers.ModelSerializer):
 
 
 class ApplicationWriteSerializer(serializers.ModelSerializer):
+    icon = serializers.ImageField(required=False)
+
+    def to_internal_value(self, data):
+        if isinstance(data, QueryDict):
+            data = dict(data.lists())
+
+        if 'name' in data and isinstance(data['name'], list):
+            data['name'] = data['name'][0]
+
+        if 'score' in data and isinstance(data['score'], list):
+            data['score'] = int(data['score'][0])
+
+        if 'description' in data and isinstance(data['description'], list):
+            data['description'] = data['description'][0]
+
+        if 'level' in data and isinstance(data['level'], list):
+            data['level'] = data['level'][0]
+
+        if 'category' in data and isinstance(data['category'], list):
+            data['category'] = int(data['category'][0])
+
+        if 'available_for_attributes' in data and isinstance(data['available_for_attributes'], list):
+            try:
+                data['available_for_attributes'] = list(map(int, data['available_for_attributes']))
+            except ValueError:
+                data['available_for_attributes'] = []
+
+        return super().to_internal_value(data)
+
     class Meta:
         model = models.Application
         fields = '__all__'
