8b1c899d00e3705c6a3f76f11a56fc31e8cf7d75
Jose Antonio Chavarría <jachavar@gmail.com>
2021-02-04 14:29:30 +0100

    Fixed api value in add_orphan_packages

--- migasfree/stats/tasks.py
+++ migasfree/stats/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -45,7 +45,13 @@ def add_orphan_packages():
             'target': 'server',
             'level': 'warning',
             'result': Package.orphan_count(),
-            'api': reverse('package-orphan'),
+            'api': json.dumps({
+                'model': 'packages',
+                'query': {
+                    'enabled': True,
+                    'store': False
+                }
+            })
         }
     )
     con.sadd('migasfree:watch:chk', 'orphan')
