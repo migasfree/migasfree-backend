26dfc5f11183d225329510f0111eefb812fe9d88
jact <jachavar@gmail.com>
2021-02-06 18:43:18 +0100

    Added STATIC_ROOT and MEDIA_ROOT settings

--- migasfree/settings/production.py
+++ migasfree/settings/production.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = ['*']
 
 MIGASFREE_PUBLIC_DIR = '/var/lib/migasfree-backend/public'
 MIGASFREE_KEYS_DIR = '/var/lib/migasfree-backend/keys'
+
+STATIC_ROOT = '/var/lib/migasfree-backend/static'
+MEDIA_ROOT = MIGASFREE_PUBLIC_DIR
 
 SECRET_KEY = secret_key(MIGASFREE_KEYS_DIR)
 
