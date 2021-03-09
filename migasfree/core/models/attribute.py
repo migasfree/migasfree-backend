e98e359112f66bb9a419276200a487f535d9457b
Jose Antonio Chavarría <jachavar@gmail.com>
2021-03-09 13:54:06 +0100

    Fixed all systems special case (process method)

--- migasfree/core/models/attribute.py
+++ migasfree/core/models/attribute.py
@@ -307,14 +307,9 @@ class BasicAttribute(Attribute):
 
         basic_attributes = []
 
-        """
         if 'SET' in properties.keys():
-            obj, _ = Attribute.objects.get_or_create(
-                property_att=Property.objects.get(pk=properties['SET']),
-                value='All Systems'
-            )
+            obj = Attribute.objects.get(pk=1)  # special case (SET-All Systems)
             basic_attributes.append(obj.id)
-        """
 
         if 'CID' in properties.keys() and 'id' in kwargs:
             description = '{}'.format(kwargs['description'])
