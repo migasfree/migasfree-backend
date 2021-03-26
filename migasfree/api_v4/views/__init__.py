da35c7a6fcbbc060880754c336d40e2c94f193bb
jact <jachavar@gmail.com>
2021-03-26 11:29:17 +0100

    Added get_computer_info

--- migasfree/api_v4/views/__init__.py
+++ migasfree/api_v4/views/__init__.py
@@ -1,2 +1,5 @@
 from .client_api import api_v4
-from .public_api import computer_label, get_key_repositories, ServerInfoView
+from .public_api import (
+    computer_label, ServerInfoView,
+    get_key_repositories, get_computer_info,
+)
