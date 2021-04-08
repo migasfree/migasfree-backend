99c20b69cdb80236a4d098ec0681739ea6265b8a
Jose Antonio Chavarría <jachavar@gmail.com>
2021-04-08 09:43:36 +0200

    Removed future absolute_import

--- migasfree/client/tasks.py
+++ migasfree/client/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import
-
 from datetime import datetime
 
 from django.db import connection
@@ -25,7 +23,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from celery import shared_task
 from celery.exceptions import Ignore
 
-from migasfree.core.models import Package
+from ..core.models import Package
 from .models import Computer
 
 
