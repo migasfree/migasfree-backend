46ff3d1f3f63651e3808ee8e365ef20c644161f2
jact <jachavar@gmail.com>
2021-04-09 11:54:22 +0200

    Added PackageHistoryStatsViewSet

--- migasfree/stats/routers.py
+++ migasfree/stats/routers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -72,6 +72,11 @@ router.register(
     r'stats/notifications',
     views.NotificationStatsViewSet,
     basename='stats-notifications'
+)
+router.register(
+    r'stats/packages-history',
+    views.PackageHistoryStatsViewSet,
+    basename='stats-packages-history'
 )
 router.register(
     r'stats/packages',
