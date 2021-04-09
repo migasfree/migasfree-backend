6929a602971a28ed9e7131c73b05f04beafeb802
jact <jachavar@gmail.com>
2021-04-09 11:54:02 +0200

    Added PackageHistoryStatsViewSet

---
+++ migasfree/stats/views/packages_history.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+
+# Copyright (c) 2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2021 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from django.db.models import Count
+from django.utils.translation import gettext as _
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import action, permission_classes
+from rest_framework.response import Response
+
+from ...client.models import PackageHistory
+
+
+@permission_classes((permissions.IsAuthenticated,))
+class PackageHistoryStatsViewSet(viewsets.ViewSet):
+    @action(methods=['get'], detail=False, url_path='project')
+    def by_project(self, request, format=None):
+        total = PackageHistory.objects.scope(request.user.userprofile).count()
+
+        data = []
+        for item in PackageHistory.objects.scope(request.user.userprofile).values(
+            'package__project__id', 'package__project__name'
+        ).annotate(
+            count=Count('package__project__id')
+        ).order_by('-count'):
+            data.append({
+                'name': item.get('package__project__name'),
+                'value': item.get('count'),
+                'project_id': item.get('package__project__id'),
+            })
+
+        return Response(
+            {
+                'title': _('Packages / Project'),
+                'total': total,
+                'data': data,
+            },
+            status=status.HTTP_200_OK
+        )
