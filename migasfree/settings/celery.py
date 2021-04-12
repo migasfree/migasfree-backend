ebdef3147899bb18a61326a39be0620186351b6d
jact <jachavar@gmail.com>
2021-04-12 13:53:46 +0200

    Added CHANNEL_LAYERS setting (Django channels)

--- migasfree/settings/celery.py
+++ migasfree/settings/celery.py
@@ -48,6 +48,15 @@ CELERYBEAT_SCHEDULE = {
     }
 }
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [(REDIS_HOST, REDIS_PORT)]
+        }
+    }
+}
+
 # django-redis
 CACHES = {
     "default": {
