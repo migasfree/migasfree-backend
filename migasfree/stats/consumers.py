17c93e5acff9098acb6c827344e9f71fe127180f
jact <jachavar@gmail.com>
2021-04-12 13:55:17 +0200

    Added consumers file (Django channels)

---
+++ migasfree/stats/consumers.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2021 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+
+class StatsConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add('stats', self.channel_name)
+        await self.accept()
+
+    async def disconnect(self):
+        await self.channel_layer.group_discard('stats', self.channel_name)
+
+    async def send_alerts(self, event):
+        await self.send_json(event['text'])
