38f178ef445996c68f216bad11f7736c78ecad7c
jact <jachavar@gmail.com>
2021-04-12 13:57:10 +0200

    Added websocket protocol (Django channels)

--- migasfree/asgi.py
+++ migasfree/asgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from .stats.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migasfree.settings.production')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(ws_urlpatterns)
+    )
+})
