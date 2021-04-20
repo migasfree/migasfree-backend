3f657dd22283a5085e4df01307343ddb3993130a
jact <jachavar@gmail.com>
2021-04-20 11:54:37 +0200

    Fixed application to other environments (production stage) (https://channels.readthedocs.io/en/latest/deploying.html#configuring-the-asgi-application)

--- migasfree/asgi.py
+++ migasfree/asgi.py
@@ -10,15 +10,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migasfree.settings.production')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from .stats.routing import ws_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migasfree.settings.production')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(ws_urlpatterns)
     )
