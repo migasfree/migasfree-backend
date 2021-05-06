80b9073cba25944de884b368f4e75ca3bf123d8f
jact <jachavar@gmail.com>
2021-05-06 10:22:52 +0200

    Added RepositoriesUrlTemplateView APIView

--- migasfree/api_v4/views/__init__.py
+++ migasfree/api_v4/views/__init__.py
@@ -1,5 +1,5 @@
 from .client_api import api_v4
 from .public_api import (
-    computer_label, ServerInfoView,
+    computer_label, ServerInfoView, RepositoriesUrlTemplateView,
     get_key_repositories, get_computer_info,
 )
