412fe3be07177a398e550e20dc5bb4b92a3964e9
jact <jachavar@gmail.com>
2021-05-10 10:00:22 +0200

    Added Pms plugins module

--- migasfree/core/pms/__init__.py
+++ migasfree/core/pms/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2017 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2017 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,18 +16,52 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+import inspect
+import importlib
+import pkgutil
+
+import migasfree.core.pms.plugins
+
 from .apt import Apt
 from .yum import Yum
 from .zypper import Zypper
 
 
+def iter_namespace(ns_pkg):
+    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + '.')
+
+
+def get_discovered_plugins():
+    return {
+        name: importlib.import_module(name)
+        for finder, name, ispkg
+        in iter_namespace(migasfree.core.pms.plugins)
+    }
+
+
 def get_available_pms():
-    return (
+    ret = [
         ('apt', 'apt'),
         ('yum', 'yum'),
         ('zypper', 'zypper'),
-    )
+    ]
+
+    discovered_plugins = get_discovered_plugins()
+    for item in discovered_plugins.keys():
+        pms = item.split('.')[-1]
+        ret.append((pms, pms))
+
+    return tuple(sorted(ret, key=lambda x: x[0]))
 
 
 def get_available_mimetypes():
-    return ';'.join(Apt().mimetype + Yum().mimetype + Zypper().mimetype)
+    ret = Apt().mimetype + Yum().mimetype + Zypper().mimetype
+
+    discovered_plugins = get_discovered_plugins()
+    for item in discovered_plugins.keys():
+        for class_ in inspect.getmembers(sys.modules[item], inspect.isclass):
+            if class_[0] != 'Pms':
+                ret += class_[1]().mimetype
+
+    return ';'.join(ret)
