a6278c65f0a1f7ee1e3a7479e34cd9ae4dc222f0
Jose Antonio Chavarría <jachavar@gmail.com>
2021-07-01 14:18:49 +0200

    Python3 adaptation

--- migasfree/core/pms/zypper.py
+++ migasfree/core/pms/zypper.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2015-2016 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2016 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,5 +25,5 @@ class Zypper(Yum):
     """
 
     def __init__(self):
-        super(Zypper, self).__init__()
+        super().__init__()
         self.name = 'zypper'
