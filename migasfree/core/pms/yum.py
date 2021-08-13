096f4ea1c98b2d039c3fe551988c75acc3f61a64
jact <jachavar@gmail.com>
2021-08-13 12:17:00 +0200

    Removed unused import

--- migasfree/core/pms/yum.py
+++ migasfree/core/pms/yum.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from ...utils import execute, get_setting
 
 from .pms import Pms
