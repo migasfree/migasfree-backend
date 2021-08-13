005ba6bd8053b6f78368a6bf14d83fde5817c736
jact <jachavar@gmail.com>
2021-08-13 12:16:31 +0200

    Removed unused import

--- migasfree/stats/management/commands/refresh_redis_syncs.py
+++ migasfree/stats/management/commands/refresh_redis_syncs.py
@@ -18,7 +18,7 @@
 
 from datetime import datetime
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django_redis import get_redis_connection
 
 from ....client.models import Synchronization
