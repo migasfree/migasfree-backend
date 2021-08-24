8c73605eec91e4e5568dc3865a6ba0f2124ed1bd
Jose Antonio Chavarría <jachavar@gmail.com>
2021-08-24 13:06:47 +0200

    PEP8 cleanup

--- migasfree/asgi.py
+++ migasfree/asgi.py
@@ -10,16 +10,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migasfree.settings.production')
-django_asgi_app = get_asgi_application()
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from .stats.routing import ws_urlpatterns
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migasfree.settings.production')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
