1296864d52bd4b59b02dc2d10582b90b907a9d97
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-21 14:40:40 +0100

    Checking if a queryset is truthy/falsey is much less efficient than checking queryset.exists()

--- migasfree/core/views/safe.py
+++ migasfree/core/views/safe.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -66,7 +66,7 @@ class SafePackageViewSet(SafePackagerConnectionMixin, viewsets.ViewSet):
                 fullname=_file.name,
                 project=project
             )
-            if package:
+            if package.exists():
                 package[0].update_store(store)
             else:
                 name, version, architecture = Package.normalized_name(_file.name)
