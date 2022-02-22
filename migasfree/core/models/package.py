6d9ccfe93eacff663c762cdd291db51e1d10b6bf
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-22 08:31:20 +0100

    When working with foreign keys, accessing the related field will result in a database read. That can be eliminated by using *_id, which is the foreign key value that Django has already cached on the object to make this scenario more efficient.

--- migasfree/core/models/package.py
+++ migasfree/core/models/package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -241,12 +241,12 @@ class Package(models.Model, MigasLink):
         if not hasattr(self, 'project'):
             return False
 
-        if self.store and self.store.project.id != self.project.id:
+        if self.store and self.store.project.id != self.project_id:
             raise ValidationError(_('Store must belong to the project'))
 
         queryset = Package.objects.filter(
             fullname=self.fullname,
-            project__id=self.project.id
+            project__id=self.project_id
         ).filter(~models.Q(id=self.id))
         if queryset.exists():
             raise ValidationError(_('Duplicated fullname at project'))
