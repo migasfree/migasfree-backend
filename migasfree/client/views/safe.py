68bdd61675f753d89b27c0f9b68dc30834579275
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-22 08:45:38 +0100

    Comparing queryset.count() is less efficient than checking queryset.exists(), so use querySet.count() if you only want the count, and use queryset.exists() if you only want to find out if at least one result exists.

--- migasfree/client/views/safe.py
+++ migasfree/client/views/safe.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -76,7 +76,7 @@ def remove_computer_messages(computer_id):
 
 def get_user_or_create(name, fullname, ip_address=None):
     user = models.User.objects.filter(name=name, fullname=fullname)
-    if not user:
+    if not user.exists():
         user = models.User.objects.create(name=name, fullname=fullname)
 
         if ip_address:
