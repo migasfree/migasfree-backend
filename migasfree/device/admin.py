7ba5bce3320bc1c39bb73560cb6092eb91993eb1
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-22 08:37:24 +0100

    Comparing queryset.count() is less efficient than checking queryset.exists(), so use querySet.count() if you only want the count, and use queryset.exists() if you only want to find out if at least one result exists.

--- migasfree/device/admin.py
+++ migasfree/device/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -150,10 +150,10 @@ class DeviceAdmin(admin.ModelAdmin):
         for capability in Capability.objects.filter(
             driver__model__id=device.model.id
         ).distinct():
-            if Logical.objects.filter(
+            if not Logical.objects.filter(
                 device__id=device.id,
                 capability=capability
-            ).count() == 0:
+            ).exists():
                 device.logical_set.create(
                     device=device,
                     capability=capability
