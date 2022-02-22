7fadc50f36840d0f713ab4511e732ee7891f32dd
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-22 08:44:42 +0100

    Comparing queryset.count() is less efficient than checking queryset.exists(), so use querySet.count() if you only want the count, and use queryset.exists() if you only want to find out if at least one result exists.

--- migasfree/client/views/public.py
+++ migasfree/client/views/public.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -34,7 +34,7 @@ from .. import permissions, models
 
 def get_platform_or_create(platform_name, ip_address=None):
     platform = Platform.objects.filter(name=platform_name)
-    if not platform:
+    if not platform.exists():
         platform = Platform.objects.create(name=platform_name)
 
         if ip_address:
