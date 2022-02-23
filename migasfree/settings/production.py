184fde4faba6054ab4f7af91b55fb18d7d766dce
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-23 08:26:34 +0100

    Not specifying encoding when reading a file can cause UnicodeDecodeError because Python assumes the file is encoded with the OS's default text encoding, but that's often an invalid assumption.

--- migasfree/settings/production.py
+++ migasfree/settings/production.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -47,6 +47,6 @@ DATABASES = {
 }
 
 try:
-    exec(open(MIGASFREE_SETTINGS_OVERRIDE).read(), globals(), locals())
+    exec(open(MIGASFREE_SETTINGS_OVERRIDE, encoding='utf_8').read(), globals(), locals())
 except IOError:
     pass
