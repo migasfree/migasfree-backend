daa60bc83fb8deaf3a1927478587c990c85e30c3
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-23 08:30:37 +0100

    A class is inherited from object by default, so explicitly inheriting from object is redundant.

--- migasfree/core/pms/pms.py
+++ migasfree/core/pms/pms.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 from ...utils import get_setting
 
 
-class Pms(object):
+class Pms:
     """
     PMS: Package Management System
     Interface class
