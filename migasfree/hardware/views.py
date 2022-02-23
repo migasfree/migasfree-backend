73b098dd08c331d113e3f1e2756a6deee6e64fe0
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-23 08:52:49 +0100

    Use of f-string

--- migasfree/hardware/views.py
+++ migasfree/hardware/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -89,7 +89,7 @@ class HardwareViewSet(
         if not name:
             name = node.description
             if node.product:
-                name = '{}: {}'.format(name, node.product)
+                name = f'{name}: {node.product}'
 
         data = {
             'name': name,
