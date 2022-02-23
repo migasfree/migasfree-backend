d1e9307fe1981e80fc5d170b50d9637a7691d897
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-23 08:29:57 +0100

    A class is inherited from object by default, so explicitly inheriting from object is redundant.

--- migasfree/core/validators.py
+++ migasfree/core/validators.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2020 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2020 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -40,7 +40,7 @@ def build_magic():
     return my_magic
 
 
-class MimetypeValidator(object):
+class MimetypeValidator:
     def __init__(self, mimetypes):
         self.mimetypes = mimetypes
 
