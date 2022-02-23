3abdc1e14cd44a1288194c0a5a96b0107be17c92
Jose Antonio Chavarría <jachavar@gmail.com>
2022-02-23 08:51:48 +0100

    Use of f-string

--- migasfree/hardware/models/capability.py
+++ migasfree/hardware/models/capability.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -57,7 +57,7 @@ class Capability(models.Model):
     def __str__(self):
         ret = self.name
         if self.description:
-            ret += ': {}'.format(self.description)
+            ret += f': {self.description}'
 
         return ret
 
