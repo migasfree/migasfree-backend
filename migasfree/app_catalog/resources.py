355e278a8cddec3824b67f95a893f126252f9fbe
jact <jachavar@gmail.com>
2022-05-26 18:02:53 +0200

    Added application catalog resources

---
+++ migasfree/app_catalog/resources.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2022 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2022 Alberto Gacías <alberto@migasfree.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from import_export import resources
+
+from .models import Application, Category, Policy
+
+
+class ApplicationResource(resources.ModelResource):
+    class Meta:
+        model = Application
+
+
+class CategoryResource(resources.ModelResource):
+    class Meta:
+        model = Category
+
+
+class PolicyResource(resources.ModelResource):
+    class Meta:
+        model = Policy
