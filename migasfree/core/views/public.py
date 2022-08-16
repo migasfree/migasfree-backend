063a6fded60e6e5fc14adf062bed77993654f681
Jose Antonio Chavarría <jachavar@gmail.com>
2022-08-16 13:27:39 +0200

    Added organization to ServerInfoView.post endpoint

--- migasfree/core/views/public.py
+++ migasfree/core/views/public.py
@@ -96,6 +96,7 @@ class ServerInfoView(views.APIView):
             'author': __author__,
             'contact': __contact__,
             'homepage': __homepage__,
+            'organization': settings.MIGASFREE_ORGANIZATION,
         }
 
         return Response(info)
