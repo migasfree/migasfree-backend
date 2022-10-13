1309ab49d366a1095b94aa65ddbcc2b8282734b1
Jose Antonio Chavarría <jachavar@gmail.com>
2022-10-13 12:35:36 +0200

    Removed unused import

--- migasfree/mixins.py
+++ migasfree/mixins.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from django.core.exceptions import ImproperlyConfigured
 from django.db import connections
 from django.db.utils import OperationalError
 
