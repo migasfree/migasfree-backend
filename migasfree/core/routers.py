9f1c968de57c94101f00960a136cdeda9aa0afae
jact <jachavar@gmail.com>
2023-06-30 13:52:05 +0200

    feat(singularity): added SingularityViewSet

--- migasfree/core/routers.py
+++ migasfree/core/routers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2023 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2023 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -30,6 +30,7 @@ router.register(r'attributes', views.AttributeViewSet)
 router.register(r'stamps', views.ServerPropertyViewSet)
 router.register(r'properties', views.PropertyViewSet)
 router.register(r'formulas', views.ClientPropertyViewSet)
+router.register(r'singularities', views.SingularityViewSet)
 router.register(r'tags', views.ServerAttributeViewSet)
 router.register(r'features', views.ClientAttributeViewSet)
 router.register(r'schedules', views.ScheduleViewSet)
