154e51210a0b1579522203369f67e66bb64bebf0
jact <jachavar@gmail.com>
2023-10-30 12:33:54 +0100

    fix: tests

--- migasfree/core/tests.py
+++ migasfree/core/tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2019 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2019 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2023 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2023 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,31 +16,31 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
-from .models import Platform
+from .models import Platform, UserProfile
 
 
 class PlatformTestCase(APITestCase):
     def setUp(self):
-        self.superuser = User.objects.create_superuser(
-            'admin', 'admin@localhost.com', 'admin'
+        self.superuser = UserProfile.objects.create(
+            username='admin', email='admin@localhost.com', password='admin',
+            is_superuser=True, is_staff=True, is_active=True,
         )
         self.client.login(username='admin', password='admin')
         self.platform = Platform.objects.create(name='Linux')
 
     def test_get_platforms(self):
         url = reverse('platform-list')
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.data[0]['name'], 'Linux')
+        response = self.client.get(url)
+        self.assertEqual(response.data['results'][0]['name'], 'Linux')
 
     def test_create_platform(self):
         url = reverse('platform-list')
         data = {'name': 'Windows'}
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_update_platform(self):
@@ -48,6 +48,7 @@ class PlatformTestCase(APITestCase):
         data = {'name': 'GNU/Linux'}
         response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['name'], data['name'])
 
     def test_delete_platform(self):
         url = reverse('platform-detail', args=[self.platform.id])
