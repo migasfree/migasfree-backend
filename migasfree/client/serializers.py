bbaac81f6d53e9b5038de2fdd4ab225c6f777535
jact <jachavar@gmail.com>
2024-02-12 13:12:55 +0100

    feat: added NotificationCreateSerializer

--- migasfree/client/serializers.py
+++ migasfree/client/serializers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2023 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2023 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -238,6 +238,12 @@ class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Notification
         fields = '__all__'
+
+
+class NotificationCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Notification
+        fields = ('message',)
 
 
 class NotificationWriteSerializer(serializers.ModelSerializer):
