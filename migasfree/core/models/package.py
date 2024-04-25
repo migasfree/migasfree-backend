f2d8d3017f5d4330f8273930a705d994d3240b66
jact <jachavar@gmail.com>
2024-04-25 20:27:05 +0200

    fix: PMS tasks are in pms module

--- migasfree/core/models/package.py
+++ migasfree/core/models/package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2023 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2023 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -291,7 +291,7 @@ class Package(models.Model, MigasLink):
 
 @receiver(pre_delete, sender=Package)
 def delete_package(sender, instance, **kwargs):
-    from .. import tasks
+    from ..pms import tasks
     from .deployment import Deployment
 
     if not instance.store:
