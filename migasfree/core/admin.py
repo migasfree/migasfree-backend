464c5670a9d4df88d122535dc7bf49b577bc1457
jact <jachavar@gmail.com>
2024-04-25 20:16:41 +0200

    fix: PMS tasks are in pms module

--- migasfree/core/admin.py
+++ migasfree/core/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2023 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2023 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@ from ..client.models import Computer
 from ..utils import cmp
 
 from .resources import AttributeResource, ProjectResource
-from . import tasks
+from .pms import tasks
 
 from .models import (
     Platform, Project, Store,
