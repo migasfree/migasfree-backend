cb17ba5dee3c6544ad4eb939285e7fe40f806632
jact <jachavar@gmail.com>
2024-04-25 20:26:58 +0200

    fix: PMS tasks are in pms module

--- migasfree/core/serializers.py
+++ migasfree/core/serializers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2023 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2023 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 from ..client.models import Computer
 from ..utils import cmp
 
-from . import tasks
+from .pms import tasks
 
 from .validators import MimetypeValidator, validate_project_pms
 from .pms import get_available_mimetypes
