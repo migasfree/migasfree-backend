e349304fb96076803db4157a1d5f60a398a2f0fe
jact <jachavar@gmail.com>
2024-07-08 09:23:39 +0200

    fix: __all__ usage to avoid unused imports

--- migasfree/__init__.py
+++ migasfree/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2021 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2021 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,3 +25,5 @@ __author__ = [
 ]
 __contact__ = "fun.with@migasfree.org"
 __homepage__ = "https://github.com/migasfree/migasfree-backend/"
+
+__all__ = ['celery_app']
