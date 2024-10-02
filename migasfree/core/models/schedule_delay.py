e75334b5543a5e68b8f5c4a16328162f2225ca9c
Jose Antonio Chavarría <jachavar@gmail.com>
2024-10-02 12:44:10 +0200

    feat: added db_comment field

--- migasfree/core/models/schedule_delay.py
+++ migasfree/core/models/schedule_delay.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015-2022 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2022 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -40,26 +40,31 @@ class ScheduleDelayManager(models.Manager):
 
 class ScheduleDelay(models.Model, MigasLink):
     delay = models.IntegerField(
-        verbose_name=_("delay")
+        verbose_name=_('delay'),
+        db_comment='number of days from the deployment start date that the assigned attributes '
+            'will be effective (Saturdays and Sundays are not taken into account)',
     )
 
     schedule = models.ForeignKey(
         Schedule,
         on_delete=models.CASCADE,
         related_name='delays',
-        verbose_name=_("schedule")
+        verbose_name=_('schedule'),
+        db_comment='related schedule',
     )
 
     attributes = models.ManyToManyField(
         Attribute,
         blank=True,
-        verbose_name=_("attributes")
+        verbose_name=_('attributes'),
+        db_comment='attributes for a delay',
     )
 
     duration = models.IntegerField(
-        verbose_name=_("duration"),
+        verbose_name=_('duration'),
         default=1,
-        validators=[MinValueValidator(1), ]
+        validators=[MinValueValidator(1), ],
+        db_comment='number of days to complete deployment to computers assigned to the delay',
     )
 
     objects = ScheduleDelayManager()
