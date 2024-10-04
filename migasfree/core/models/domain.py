c1d52fda4ada59816df02227c0f603cc567bed01
jact <jachavar@gmail.com>
2024-10-04 13:43:50 +0200

    feat: added db_table_comment field

--- migasfree/core/models/domain.py
+++ migasfree/core/models/domain.py
@@ -156,6 +156,7 @@ class Domain(models.Model, MigasLink):
         app_label = 'core'
         verbose_name = _('Domain')
         verbose_name_plural = _('Domains')
+        db_table_comment = 'group of computers that are jointly administered'
 
 
 @receiver(post_save, sender=Domain)
