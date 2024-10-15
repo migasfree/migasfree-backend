4e9afba9f69898279f413087d96a61d240e546a8
Jose Antonio Chavarría <jachavar@gmail.com>
2024-10-15 09:02:20 +0200

    fix: scope method

--- migasfree/client/models/package_history.py
+++ migasfree/client/models/package_history.py
@@ -35,7 +35,7 @@ class PackageHistoryManager(models.Manager):
 
     def scope(self, user):
         qs = self.get_queryset()
-        if not user.is_view_all():
+        if user and not user.is_view_all():
             qs = qs.filter(computer__in=user.get_computers())
 
         return qs
