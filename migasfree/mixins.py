6f3cb6be7e489264f526c603470d10482b91e8a9
Jose Antonio Chavarría <jachavar@gmail.com>
2024-10-16 09:17:09 +0200

    fix: information exposure through an exception

--- migasfree/mixins.py
+++ migasfree/mixins.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2022 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2022 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2022-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2022-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,11 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+
 from django.db import connections
 from django.db.utils import OperationalError
 
 from rest_framework import status, viewsets
 from rest_framework.response import Response
+
+logger = logging.getLogger('migasfree')
 
 
 class DatabaseCheckMixin(viewsets.ViewSet):
@@ -29,8 +33,9 @@ class DatabaseCheckMixin(viewsets.ViewSet):
             connections['default'].cursor()
             return super().dispatch(request, *args, **kwargs)
         except OperationalError as e:
+            logger.error('Operational error in database: %s', e)
             response = Response(
-                '{}: {}'.format('Operational error in database', e),
+                'Operational error in database',
                 status=status.HTTP_503_SERVICE_UNAVAILABLE
             )
             if not getattr(request, 'accepted_renderer', None):
