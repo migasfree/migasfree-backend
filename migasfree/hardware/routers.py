78a59699d869fc7781db80ac14b19dc1d1b73c99
Jose Antonio Chavarría <jachavar@gmail.com>
2024-10-23 11:59:11 +0200

    fix: removed unused class

--- migasfree/hardware/routers.py
+++ migasfree/hardware/routers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 *-*
 
-# Copyright (c) 2015-2018 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2018 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2024 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2024 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,11 +22,6 @@ from . import views
 
 router = routers.DefaultRouter()
 
-router.register(
-    r'computers',
-    views.HardwareComputerViewSet,
-    basename='computers-hardware'
-)
 router.register(r'hardware', views.HardwareViewSet)
 
 safe_router = routers.DefaultRouter()
