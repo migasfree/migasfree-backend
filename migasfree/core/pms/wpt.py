51f555973a21c81271701d3b062ebd19c3ffcfac
Jose Antonio Chavarría <jachavar@gmail.com>
2025-04-10 13:47:17 +0200

    refactor: removed unused code (is_package_in_repo method)

--- migasfree/core/pms/wpt.py
+++ migasfree/core/pms/wpt.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2024 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2024 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2024-2025 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2024-2025 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -199,9 +199,3 @@ class Wpt(Pms):
                     )
 
         return ''
-
-    def is_package_in_repo(self, filename, path):
-        cmd = f'grep -r {filename} {path}/*'
-        ret, _, _ = execute(cmd)
-
-        return ret == 0
