4277780d81aaa20b91738db2a52c10bc11dea21f
Jose Antonio Chavarría <jachavar@gmail.com>
2025-06-17 12:25:32 +0200

    fix: incremented throttle rates

--- migasfree/settings/base.py
+++ migasfree/settings/base.py
@@ -151,11 +151,11 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
+        'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/hour',
-        'user': '1000/minute'
+        'anon': '1000/second',
+        'user': '1000/second',
     },
 }
 
