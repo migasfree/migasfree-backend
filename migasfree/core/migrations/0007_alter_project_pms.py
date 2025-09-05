a26ccc89736bb15cdbe4d3c5286b5920493c7d3f
jact <jachavar@gmail.com>
2025-09-05 09:49:48 +0200

    feat: removed winget pms (because it's impossible create private repos in migasfree server)

---
+++ migasfree/core/migrations/0007_alter_project_pms.py
@@ -0,0 +1,30 @@
+import migasfree.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0006_deployment_project'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='project',
+            name='pms',
+            field=models.CharField(
+                choices=[
+                    ('apt', 'apt'),
+                    ('dnf', 'dnf'),
+                    ('pacman', 'pacman'),
+                    ('wpt', 'wpt'),
+                    ('yum', 'yum'),
+                    ('zypper', 'zypper')
+                ],
+                db_comment="package management system utilized in the project's operating system",
+                max_length=50,
+                validators=[migasfree.core.validators.validate_project_pms],
+                verbose_name='package management system'
+            ),
+        ),
+    ]
