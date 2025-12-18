7a2c1c3e4d088bb70de08ef4f1b19f1c9cd5e45e
Jose Antonio Chavarría <jachavar@gmail.com>
2025-12-18 12:57:41 +0100

    refactor: code

--- migasfree/core/models/deployment.py
+++ migasfree/core/models/deployment.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # Copyright (c) 2015-2025 Jose Antonio Chavarría <jachavar@gmail.com>
 # Copyright (c) 2015-2025 Alberto Gacías <alberto@migasfree.org>
 #
@@ -16,39 +14,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
 import os
 import shutil
-import datetime
 
 from django.conf import settings
-from django.db import models
-from django.db.models import Q
-from django.db.models.signals import pre_save, pre_delete
-from django.dispatch import receiver
 from django.core.exceptions import ValidationError
+from django.db import models
+from django.db.models import F, Q
+from django.db.models.functions import Mod
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
 from django.template.defaultfilters import slugify
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django_redis import get_redis_connection
 
+from ...utils import normalize_line_breaks, time_horizon
 from ..pms import get_pms
-from ...utils import time_horizon, normalize_line_breaks
-
+from .attribute import Attribute
+from .domain import Domain
 from .migas_link import MigasLink
-from .project import Project
 from .package import Package
 from .package_set import PackageSet
-from .attribute import Attribute
+from .project import Project
 from .schedule import Schedule
-from .schedule_delay import ScheduleDelay
-from .domain import Domain
 
 
 class DeploymentManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().select_related(
-            'project', 'schedule', 'domain'
-        )
+        return super().get_queryset().select_related('project', 'schedule', 'domain')
 
     def scope(self, user):
         qs = self.get_queryset()
@@ -118,7 +113,7 @@ class Deployment(models.Model, MigasLink):
         blank=True,
         help_text=_('Mandatory packages to install each time'),
         db_comment='lists the packages that will be automatically installed'
-                   ' when a computer\'s attributes match the deployment',
+        " when a computer's attributes match the deployment",
     )
 
     packages_to_remove = models.TextField(
@@ -127,7 +122,7 @@ class Deployment(models.Model, MigasLink):
         blank=True,
         help_text=_('Mandatory packages to remove each time'),
         db_comment='lists the packages that will be automatically removed when'
-                   ' a computer\'s attributes match the deployment',
+        " a computer's attributes match the deployment",
     )
 
     included_attributes = models.ManyToManyField(
@@ -163,7 +158,7 @@ class Deployment(models.Model, MigasLink):
         verbose_name=_('auto restart'),
         default=False,
         db_comment='indicates that start date is updated once the deployment is complete,'
-                   ' ensuring an automatic restart of the process',
+        ' ensuring an automatic restart of the process',
     )
 
     default_preincluded_packages = models.TextField(
@@ -253,8 +248,8 @@ class Deployment(models.Model, MigasLink):
     expire = models.IntegerField(
         verbose_name=_('metadata cache minutes. Default 1440 minutes = 1 day'),
         default=1440,  # 60m * 24h = 1 day
-        db_comment='minutes that the public repository\'s metadata will remain cached'
-                   ' (only taken into account in the case where the frozen is false)',
+        db_comment="minutes that the public repository's metadata will remain cached"
+        ' (only taken into account in the case where the frozen is false)',
     )
 
     objects = DeploymentManager()
@@ -266,8 +261,7 @@ class Deployment(models.Model, MigasLink):
     def get_percent(begin_date, end_date):
         delta = end_date - begin_date
         aware_date = timezone.make_aware(
-            datetime.datetime.combine(begin_date, datetime.datetime.min.time()),
-            timezone.get_default_timezone()
+            datetime.datetime.combine(begin_date, datetime.datetime.min.time()), timezone.get_default_timezone()
         )
         progress = timezone.localtime(timezone.now()) - aware_date
 
@@ -286,23 +280,19 @@ class Deployment(models.Model, MigasLink):
         if self.schedule is None:
             return None
 
-        delays = ScheduleDelay.objects.filter(
-            schedule__id=self.schedule_id
-        ).order_by('delay')
+        # Use related manager to benefit from prefetch_related if available
+        delays = list(self.schedule.delays.order_by('delay'))
 
-        if not delays.exists():
+        if not delays:
             return None
 
         begin_date = time_horizon(self.start_date, delays[0].delay)
-        end_date = time_horizon(
-            self.start_date,
-            delays.reverse()[0].delay + delays.reverse()[0].duration
-        )
+        end_date = time_horizon(self.start_date, delays[-1].delay + delays[-1].duration)
 
         return {
             'begin_date': str(begin_date),
             'end_date': str(end_date),
-            'percent': self.get_percent(begin_date, end_date)
+            'percent': self.get_percent(begin_date, end_date),
         }
 
     def timeline(self):
@@ -311,15 +301,9 @@ class Deployment(models.Model, MigasLink):
         if not schedule_timeline:
             return None
 
-        date_format = "%Y-%m-%d"
-        begin_date = datetime.datetime.strptime(
-            schedule_timeline['begin_date'],
-            date_format
-        )
-        end_date = datetime.datetime.strptime(
-            schedule_timeline['end_date'],
-            date_format
-        )
+        date_format = '%Y-%m-%d'
+        begin_date = datetime.datetime.strptime(schedule_timeline['begin_date'], date_format)
+        end_date = datetime.datetime.strptime(schedule_timeline['end_date'], date_format)
 
         days = (datetime.datetime.today() - begin_date).days + 1
         total_days = (end_date - begin_date).days
@@ -327,12 +311,8 @@ class Deployment(models.Model, MigasLink):
             'deployment_id': self.pk,
             'percent': schedule_timeline['percent'],
             'schedule': self.schedule,
-            'info': _('%s/%s days (from %s to %s)') % (
-                days,
-                total_days,
-                schedule_timeline['begin_date'],
-                schedule_timeline['end_date']
-            )
+            'info': _('%s/%s days (from %s to %s)')
+            % (days, total_days, schedule_timeline['begin_date'], schedule_timeline['end_date']),
         }
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
@@ -353,6 +333,7 @@ class Deployment(models.Model, MigasLink):
 
         try:
             from ...stats import tasks
+
             tasks.assigned_computers_to_deployment(self.id)
         except Exception:
             pass
@@ -363,49 +344,52 @@ class Deployment(models.Model, MigasLink):
         Return available deployments for a computer and attributes list
         """
         # first: all deployments by attribute
-        attributed = Deployment.objects.filter(
-            project__id=computer.project.id,
-            enabled=True,
-            included_attributes__id__in=attributes,
-            start_date__lte=timezone.localtime(timezone.now()).date()
-        ).filter(
-            Q(domain__isnull=True) | (
-                Q(domain__included_attributes__id__in=attributes) &
-                ~Q(domain__excluded_attributes__id__in=attributes)
+        attributed = (
+            Deployment.objects.filter(
+                project__id=computer.project.id,
+                enabled=True,
+                included_attributes__id__in=attributes,
+                start_date__lte=timezone.localtime(timezone.now()).date(),
             )
-        ).values_list('id', flat=True)
+            .filter(
+                Q(domain__isnull=True)
+                | (
+                    Q(domain__included_attributes__id__in=attributes)
+                    & ~Q(domain__excluded_attributes__id__in=attributes)
+                )
+            )
+            .values_list('id', flat=True)
+        )
         lst = list(attributed)
 
         # second: all deployments by schedule
-        scheduled = Deployment.objects.filter(
-            project__id=computer.project.id,
-            enabled=True,
-            schedule__delays__attributes__id__in=attributes
-        ).filter(
-            Q(domain__isnull=True) | (
-                Q(domain__included_attributes__id__in=attributes) &
-                ~Q(domain__excluded_attributes__id__in=attributes)
+        scheduled = (
+            Deployment.objects.filter(
+                project__id=computer.project.id, enabled=True, schedule__delays__attributes__id__in=attributes
             )
-        ).extra(
-            select={
-                'delay': 'core_scheduledelay.delay',
-                'duration': 'core_scheduledelay.duration'
-            }
+            .filter(
+                Q(domain__isnull=True)
+                | (
+                    Q(domain__included_attributes__id__in=attributes)
+                    & ~Q(domain__excluded_attributes__id__in=attributes)
+                )
+            )
+            .annotate(delay=F('schedule__delays__delay'), duration=F('schedule__delays__duration'))
         )
 
         for deploy in scheduled:
             for duration in range(0, deploy.duration):
-                if computer.id % deploy.duration == duration:
-                    if time_horizon(
-                        deploy.start_date, deploy.delay + duration
-                    ) <= timezone.localtime(timezone.now()).date():
-                        lst.append(deploy.id)
-                        break
+                if computer.id % deploy.duration == duration and (
+                    time_horizon(deploy.start_date, deploy.delay + duration)
+                    <= timezone.localtime(timezone.now()).date()
+                ):
+                    lst.append(deploy.id)
+                    break
 
         # 3.- excluded attributes
-        deployments = Deployment.objects.filter(id__in=lst).filter(
-            ~Q(excluded_attributes__id__in=attributes)
-        ).order_by('name')
+        deployments = (
+            Deployment.objects.filter(id__in=lst).filter(~Q(excluded_attributes__id__in=attributes)).order_by('name')
+        )
 
         return deployments
 
@@ -413,17 +397,16 @@ class Deployment(models.Model, MigasLink):
         """
         Returns Queryset with the related computers based in attributes and schedule
         """
-        if model != 'computer' or not self.enabled \
-                or self.start_date > timezone.localtime(timezone.now()).date():
+        if model != 'computer' or not self.enabled or self.start_date > timezone.localtime(timezone.now()).date():
             return None
 
         from ...client.models import Computer
 
         # by assigned attributes
-        computers = Computer.productive.scope(user).filter(
-            project_id=self.project_id
-        ).filter(
-            Q(sync_attributes__in=self.included_attributes.all())
+        computers = (
+            Computer.productive.scope(user)
+            .filter(project_id=self.project_id)
+            .filter(Q(sync_attributes__in=self.included_attributes.all()))
         )
 
         # by schedule
@@ -431,24 +414,23 @@ class Deployment(models.Model, MigasLink):
             for delay in self.schedule.delays.all():
                 delay_attributes = list(delay.attributes.values_list('id', flat=True))
                 for duration in range(0, delay.duration):
-                    if time_horizon(
-                        self.start_date, delay.delay + duration
-                    ) <= timezone.localtime(timezone.now()).date():
-                        computers_schedule = Computer.productive.scope(user).filter(
-                            project_id=self.project_id
-                        ).filter(
-                            Q(sync_attributes__id__in=delay_attributes)
-                        ).extra(
-                            where=[f'MOD(client_computer.id, {delay.duration}) = {duration}']
+                    if (
+                        time_horizon(self.start_date, delay.delay + duration)
+                        <= timezone.localtime(timezone.now()).date()
+                    ):
+                        computers_schedule = (
+                            Computer.productive.scope(user)
+                            .filter(project_id=self.project_id)
+                            .filter(Q(sync_attributes__id__in=delay_attributes))
+                            .annotate(mod_duration=Mod('id', delay.duration))
+                            .filter(mod_duration=duration)
                         )
                         computers |= computers_schedule
                     else:
                         break
 
         # excluded attributes
-        computers = computers.exclude(
-            Q(sync_attributes__in=self.excluded_attributes.all())
-        )
+        computers = computers.exclude(Q(sync_attributes__in=self.excluded_attributes.all()))
 
         return computers.distinct()
 
@@ -461,16 +443,16 @@ class Deployment(models.Model, MigasLink):
             self.pms().relative_path
             if self.source == Deployment.SOURCE_INTERNAL
             else settings.MIGASFREE_EXTERNAL_TRAILING_PATH,
-            name if name else self.slug
+            name if name else self.slug,
         )
 
     def source_template(self):
         return self.pms().source_template(self)
 
     def can_delete(self, user):
-        return user.has_perm('core.delete_deployment') and \
-           (not user.userprofile.domains.count() or
-               self.domain == user.userprofile.domain_preference)
+        return user.has_perm('core.delete_deployment') and (
+            not user.userprofile.domains.count() or self.domain == user.userprofile.domain_preference
+        )
 
     class Meta:
         app_label = 'core'
@@ -489,11 +471,13 @@ def pre_save_deployment(sender, instance, **kwargs):
         if old_obj.project_id != instance.project.id:
             raise ValidationError(_('Is not allowed change project'))
 
-        if instance.available_packages != old_obj.available_packages \
-                or instance.packages_to_install != old_obj.packages_to_install \
-                or instance.packages_to_remove != old_obj.packages_to_remove:
+        if (
+            instance.available_packages != old_obj.available_packages
+            or instance.packages_to_install != old_obj.packages_to_install
+            or instance.packages_to_remove != old_obj.packages_to_remove
+        ):
             con = get_redis_connection()
-            con.delete('migasfree:deployments:%d:computers' % instance.id)
+            con.delete(f'migasfree:deployments:{instance.id}:computers')
 
 
 @receiver(pre_delete, sender=Deployment)
@@ -503,7 +487,7 @@ def pre_delete_deployment(sender, instance, **kwargs):
         shutil.rmtree(path)
 
     con = get_redis_connection()
-    con.delete('migasfree:deployments:%d:computers' % instance.id)
+    con.delete(f'migasfree:deployments:{instance.id}:computers')
 
 
 class InternalSourceManager(DeploymentManager):
