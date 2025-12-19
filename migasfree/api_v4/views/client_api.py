229da410a449158f4bb09ce18d5600a5e24ab837
Jose Antonio Chavarría <jachavar@gmail.com>
2025-12-19 08:14:32 +0100

    feat: added logging in generic exception

--- migasfree/api_v4/views/client_api.py
+++ migasfree/api_v4/views/client_api.py
@@ -1,28 +1,41 @@
-# -*- coding: utf-8 -*-
-
-import os
 import json
+import logging
+import os
 
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils.translation import gettext as _
 from django.views.decorators.csrf import csrf_exempt
 
-from ..api import (
-    get_computer, return_message, save_request_file,
-    register_computer, get_key_packager,
-    upload_server_package, upload_server_set, create_repositories_of_packageset,
-    upload_computer_message, get_properties, upload_computer_info,
-    upload_computer_faults, upload_computer_hardware,
-    upload_computer_software_base, upload_computer_software_base_diff,
-    upload_computer_software_history, get_computer_software,
-    upload_computer_errors, upload_devices_changes,
-    set_computer_tags, get_computer_tags,
-)
 from ...client.models import Error, Notification
-from ..secure import wrap, unwrap
-from ...utils import get_client_ip, uuid_validate, read_file
+from ...utils import get_client_ip, read_file, uuid_validate
 from .. import errmfs
+from ..api import (
+    create_repositories_of_packageset,
+    get_computer,
+    get_computer_software,
+    get_computer_tags,
+    get_key_packager,
+    get_properties,
+    register_computer,
+    return_message,
+    save_request_file,
+    set_computer_tags,
+    upload_computer_errors,
+    upload_computer_faults,
+    upload_computer_hardware,
+    upload_computer_info,
+    upload_computer_message,
+    upload_computer_software_base,
+    upload_computer_software_base_diff,
+    upload_computer_software_history,
+    upload_devices_changes,
+    upload_server_package,
+    upload_server_set,
+)
+from ..secure import unwrap, wrap
+
+logger = logging.getLogger('migasfree')
 
 # USING USERNAME AND PASSWORD ONLY (WITHOUT KEYS PAIR)
 API_REGISTER = {
@@ -90,28 +103,23 @@ def get_msg_info(text):
 def api_v4(request):
     if not check_tmp_path():
         return HttpResponse(
-            return_message('temporal_path_not_created', errmfs.error(errmfs.GENERIC)),
-            content_type='text/plain'
+            return_message('temporal_path_not_created', errmfs.error(errmfs.GENERIC)), content_type='text/plain'
         )
 
     if request.method != 'POST':
         return HttpResponse(
             return_message('unexpected_get_method', errmfs.error(errmfs.GET_METHOD_NOT_ALLOWED)),
-            content_type='text/plain'
+            content_type='text/plain',
         )
 
     msg = request.FILES.get('message')
     if not msg:
-        return HttpResponse(
-            return_message('no_message_file', errmfs.error(errmfs.GENERIC)),
-            content_type='text/plain'
-        )
+        return HttpResponse(return_message('no_message_file', errmfs.error(errmfs.GENERIC)), content_type='text/plain')
 
     filename = os.path.normpath(os.path.join(settings.MIGASFREE_TMP_DIR, msg.name))
     if not filename.startswith(settings.MIGASFREE_TMP_DIR):
         return HttpResponse(
-            return_message('invalid_file_path', errmfs.error(errmfs.GENERIC)),
-            content_type='text/plain'
+            return_message('invalid_file_path', errmfs.error(errmfs.GENERIC)), content_type='text/plain'
         )
 
     filename_return = f'{filename}.return'
@@ -123,22 +131,13 @@ def api_v4(request):
         Error.objects.create(
             computer,
             computer.project,
-            f"{get_client_ip(request)} - {command} - {errmfs.error_info(errmfs.UNSUBSCRIBED_COMPUTER)}"
+            f'{get_client_ip(request)} - {command} - {errmfs.error_info(errmfs.UNSUBSCRIBED_COMPUTER)}',
         )
-        ret = return_message(
-            command,
-            errmfs.error(errmfs.UNSUBSCRIBED_COMPUTER)
-        )
-        return HttpResponse(
-            wrap_command_result(filename_return, ret),
-            content_type='text/plain'
-        )
+        ret = return_message(command, errmfs.error(errmfs.UNSUBSCRIBED_COMPUTER))
+        return HttpResponse(wrap_command_result(filename_return, ret), content_type='text/plain')
 
     if computer and computer.status == 'available' and command == 'upload_computer_info':
-        Notification.objects.create(
-            _('Computer [%s] with available status, has been synchronized')
-            % computer
-        )
+        Notification.objects.create(_('Computer [%s] with available status, has been synchronized') % computer)
 
     # COMPUTERS
     if command in API_PROJECT:  # IF COMMAND IS BY PROJECT
@@ -153,7 +152,7 @@ def api_v4(request):
                     Error.objects.create(
                         computer,
                         computer.project,
-                        f"{get_client_ip(request)} - {command} - {errmfs.error_info(errmfs.INVALID_SIGNATURE)}"
+                        f'{get_client_ip(request)} - {command} - {errmfs.error_info(errmfs.INVALID_SIGNATURE)}',
                     )
             else:
                 handler = API_PROJECT.get(command)
@@ -166,10 +165,7 @@ def api_v4(request):
         else:
             ret = return_message(command, errmfs.error(errmfs.COMPUTER_NOT_FOUND))
 
-        return HttpResponse(
-            wrap_command_result(filename_return, ret),
-            content_type='text/plain'
-        )
+        return HttpResponse(wrap_command_result(filename_return, ret), content_type='text/plain')
 
     # REGISTERS
     # COMMAND NOT USE KEYS PAIR, ONLY USERNAME AND PASSWORD
@@ -185,7 +181,8 @@ def api_v4(request):
                 ret = handler(request, name, uuid, computer, data)
             else:
                 ret = return_message(command, errmfs.error(errmfs.COMMAND_NOT_FOUND))
-        except Exception:
+        except Exception as e:
+            logger.error('Error in register API command %s: %s', command, e)
             ret = return_message(command, errmfs.error(errmfs.GENERIC))
 
         os.remove(filename)
@@ -208,13 +205,7 @@ def api_v4(request):
 
         os.remove(filename)
 
-        return HttpResponse(
-            wrap_command_result(filename_return, ret),
-            content_type='text/plain'
-        )
+        return HttpResponse(wrap_command_result(filename_return, ret), content_type='text/plain')
 
     else:
-        return HttpResponse(
-            return_message(command, errmfs.error(errmfs.COMMAND_NOT_FOUND)),
-            content_type='text/plain'
-        )
+        return HttpResponse(return_message(command, errmfs.error(errmfs.COMMAND_NOT_FOUND)), content_type='text/plain')
