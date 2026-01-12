333a62504a25e8ef17f4f8f224f55d22959e87d0
jact <jachavar@gmail.com>
2026-01-12 12:44:24 +0100

    refactor: ruff guidelines

--- migasfree/api_v4/views/__init__.py
+++ migasfree/api_v4/views/__init__.py
@@ -1,11 +1,17 @@
 from .client_api import api_v4
 from .public_api import (
-    computer_label, ServerInfoView, RepositoriesUrlTemplateView,
-    get_key_repositories, get_computer_info,
+    RepositoriesUrlTemplateView,
+    ServerInfoView,
+    computer_label,
+    get_computer_info,
+    get_key_repositories,
 )
 
 __all__ = [
+    'RepositoriesUrlTemplateView',
+    'ServerInfoView',
     'api_v4',
-    'computer_label', 'ServerInfoView', 'RepositoriesUrlTemplateView',
-    'get_key_repositories', 'get_computer_info',
+    'computer_label',
+    'get_computer_info',
+    'get_key_repositories',
 ]
