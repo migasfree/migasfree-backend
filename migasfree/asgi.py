310eb9161b68d78ae9090800ef64cb211357370a
jact <jachavar@gmail.com>
2026-01-12 12:28:29 +0100

    refactor: ruff guidelines

--- migasfree/asgi.py
+++ migasfree/asgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 from .stats.routing import ws_urlpatterns
 
