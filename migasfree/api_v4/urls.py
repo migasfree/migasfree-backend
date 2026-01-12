b61b525e71780bd54f5187d8609a76baec93380d
jact <jachavar@gmail.com>
2026-01-12 12:43:24 +0100

    refactor: ruff guidelines

--- migasfree/api_v4/urls.py
+++ migasfree/api_v4/urls.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 *-*
-
 from django.urls import re_path
 
 from .views import (
-    api_v4, computer_label, ServerInfoView, RepositoriesUrlTemplateView,
-    get_key_repositories, get_computer_info,
+    RepositoriesUrlTemplateView,
+    ServerInfoView,
+    api_v4,
+    computer_label,
+    get_computer_info,
+    get_key_repositories,
 )
-
 
 urlpatterns = [
     re_path(r'^api/$', api_v4, name='api_v4'),
@@ -15,16 +16,8 @@ urlpatterns = [
         computer_label,
         name='computer_label',
     ),
-    re_path(
-        r'^get_key_repositories/$',
-        get_key_repositories,
-        name='get_key_repositories'
-    ),
-    re_path(
-        r'^get_computer_info/$',
-        get_computer_info,
-        name='get_computer_info'
-    ),
+    re_path(r'^get_key_repositories/$', get_key_repositories, name='get_key_repositories'),
+    re_path(r'^get_computer_info/$', get_computer_info, name='get_computer_info'),
     re_path(r'^api/v1/public/repository-url-template/', RepositoriesUrlTemplateView.as_view()),
     re_path(r'^api/v1/public/server/info/', ServerInfoView.as_view()),
 ]
