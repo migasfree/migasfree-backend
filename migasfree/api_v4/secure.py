4a08db94ad826bd4e00539a9dc4efdc9e012b96b
jact <jachavar@gmail.com>
2026-01-12 12:43:19 +0100

    refactor: ruff guidelines

--- migasfree/api_v4/secure.py
+++ migasfree/api_v4/secure.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
-
+import json
 import os
 import re
-import json
 import subprocess
 
 from django.conf import settings
 
-from . import errmfs
-from ..utils import read_file, write_file
 from ..secure import create_server_keys, generate_rsa_keys
+from ..utils import read_file, write_file
+from . import errmfs
 
 SIGN_LEN = 256
 
@@ -17,58 +15,60 @@ SIGN_LEN = 256
 def is_safe_filename(filename):
     # Check for any dangerous patterns or reserved filenames
     dangerous_patterns = ['..', '\\', '|', ';', '&', '$', '>', '<']
-    reserved_filenames = [
-        r'^/dev/', r'^/proc/', r'^/sys/',
-        r'^lost\+found.*$', r'^\.trash-.*', r'^\.Trash-.*$'
-    ]
+    reserved_filenames = [r'^/dev/', r'^/proc/', r'^/sys/', r'^lost\+found.*$', r'^\.trash-.*', r'^\.Trash-.*$']
 
     for pattern in dangerous_patterns:
         if pattern in filename:
             return False
 
-    for reserved_filename in reserved_filenames:
-        if re.match(reserved_filename, filename):
-            return False
-
-    return True
+    return all(not re.match(reserved_filename, filename) for reserved_filename in reserved_filenames)
 
 
 def sign(filename):
     if not is_safe_filename(filename):
-        print(f"Invalid filename: {filename}")
+        print(f'Invalid filename: {filename}')
         return False
 
     command = [
-        'openssl', 'dgst', '-sha1', '-sign',
+        'openssl',
+        'dgst',
+        '-sha1',
+        '-sign',
         os.path.join(settings.MIGASFREE_KEYS_DIR, 'migasfree-server.pri'),
-        '-out', f'{filename}.sign', filename
+        '-out',
+        f'{filename}.sign',
+        filename,
     ]
 
     try:
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError as e:
-        print(f"Error signing file: {e}")
+        print(f'Error signing file: {e}')
 
 
 def verify(filename, key):
     # returns True if OK, False otherwise
 
     if not is_safe_filename(filename):
-        print(f"Invalid filename: {filename}")
+        print(f'Invalid filename: {filename}')
         return False
 
     command = [
-        'openssl', 'dgst', '-sha1', '-verify',
+        'openssl',
+        'dgst',
+        '-sha1',
+        '-verify',
         os.path.join(settings.MIGASFREE_KEYS_DIR, f'{key}.pub'),
-        '-signature', f'{filename}.sign',
-        filename
+        '-signature',
+        f'{filename}.sign',
+        filename,
     ]
 
     try:
         subprocess.run(command, capture_output=True, text=True, check=True)
         return True
     except subprocess.CalledProcessError as e:
-        print(f"Error during verification: {e.stderr}")
+        print(f'Error during verification: {e.stderr}')
         return False
 
 
@@ -78,7 +78,7 @@ def wrap(filename, data):
     """
 
     if not is_safe_filename(filename):
-        print(f"Invalid filename: {filename}")
+        print(f'Invalid filename: {filename}')
         return False
 
     data = json.dumps(data)
@@ -88,9 +88,8 @@ def wrap(filename, data):
 
     sign(filename)
 
-    with open(filename, 'ab') as fp:
-        with open(f'{filename}.sign', 'rb') as fpsign:
-            fp.write(fpsign.read())
+    with open(filename, 'ab') as fp, open(f'{filename}.sign', 'rb') as fpsign:
+        fp.write(fpsign.read())
 
     os.remove(f'{filename}.sign')
 
@@ -101,7 +100,7 @@ def unwrap(filename, key):
     """
 
     if not is_safe_filename(filename):
-        print(f"Invalid filename: {filename}")
+        print(f'Invalid filename: {filename}')
         return False
 
     with open(filename, 'rb') as fp:
@@ -109,8 +108,11 @@ def unwrap(filename, key):
 
     n = len(content)
 
-    write_file(f'{filename}.sign', content[n - SIGN_LEN:n])
-    write_file(filename, content[0:n - SIGN_LEN])
+    if n < SIGN_LEN:
+        return errmfs.error(errmfs.INVALID_SIGNATURE)
+
+    write_file(f'{filename}.sign', content[n - SIGN_LEN : n])
+    write_file(filename, content[0 : n - SIGN_LEN])
 
     if verify(filename, key):
         with open(filename, 'rb') as f:
@@ -127,46 +129,32 @@ def get_keys_to_client(project):
     """
     Returns the keys for register computer
     """
-    priv_project_key_file = os.path.join(
-        settings.MIGASFREE_KEYS_DIR, f'{project}.pri'
-    )
+    priv_project_key_file = os.path.join(settings.MIGASFREE_KEYS_DIR, f'{project}.pri')
     if not os.path.exists(priv_project_key_file):
         generate_rsa_keys(project)
 
-    pub_server_key_file = os.path.join(
-        settings.MIGASFREE_KEYS_DIR, settings.MIGASFREE_PUBLIC_KEY
-    )
+    pub_server_key_file = os.path.join(settings.MIGASFREE_KEYS_DIR, settings.MIGASFREE_PUBLIC_KEY)
     if not os.path.exists(pub_server_key_file):
         create_server_keys()
 
     pub_server_key = read_file(pub_server_key_file)
     priv_project_key = read_file(priv_project_key_file)
 
-    return {
-        settings.MIGASFREE_PUBLIC_KEY: pub_server_key.decode(),
-        'migasfree-client.pri': priv_project_key.decode()
-    }
+    return {settings.MIGASFREE_PUBLIC_KEY: pub_server_key.decode(), 'migasfree-client.pri': priv_project_key.decode()}
 
 
 def get_keys_to_packager():
     """
     Returns the keys for register packager
     """
-    pub_server_key_file = os.path.join(
-        settings.MIGASFREE_KEYS_DIR, settings.MIGASFREE_PUBLIC_KEY
-    )
+    pub_server_key_file = os.path.join(settings.MIGASFREE_KEYS_DIR, settings.MIGASFREE_PUBLIC_KEY)
     if not os.path.exists(pub_server_key_file):
         create_server_keys()
 
     pub_server_key = read_file(pub_server_key_file)
-    priv_packager_key = read_file(
-        os.path.join(
-            settings.MIGASFREE_KEYS_DIR,
-            settings.MIGASFREE_PACKAGER_PRI_KEY
-        )
-    )
+    priv_packager_key = read_file(os.path.join(settings.MIGASFREE_KEYS_DIR, settings.MIGASFREE_PACKAGER_PRI_KEY))
 
     return {
         settings.MIGASFREE_PUBLIC_KEY: pub_server_key.decode(),
-        settings.MIGASFREE_PACKAGER_PRI_KEY: priv_packager_key.decode()
+        settings.MIGASFREE_PACKAGER_PRI_KEY: priv_packager_key.decode(),
     }
