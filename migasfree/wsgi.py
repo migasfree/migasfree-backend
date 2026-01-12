172ec5730e37d68c4702f1aec07b66486b33827a
jact <jachavar@gmail.com>
2026-01-12 12:32:12 +0100

    refactor: ruff guidelines

--- migasfree/wsgi.py
+++ migasfree/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import psutil
 
+import psutil
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "migasfree.settings.production")
