e503674c07a53fb85de1fbb6be1beb0e39f05715
jact <jachavar@gmail.com>
2026-01-13 18:48:02 +0100

    fix: import

--- migasfree/client/views/token.py
+++ migasfree/client/views/token.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2015-2025 Jose Antonio Chavarría <jachavar@gmail.com>
-# Copyright (c) 2015-2025 Alberto Gacías <alberto@migasfree.org>
+# Copyright (c) 2015-2026 Jose Antonio Chavarría <jachavar@gmail.com>
+# Copyright (c) 2015-2026 Alberto Gacías <alberto@migasfree.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -59,7 +59,7 @@ from ..filters import (
     SynchronizationFilter,
     UserFilter,
 )
-from .safe import remove_computer_messages
+from ..messages import remove_computer_messages
 
 
 @extend_schema(tags=['computers'])
