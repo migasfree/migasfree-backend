eae5e31d3f0096eecd5cfe727f16bd31f35a0890
jact <jachavar@gmail.com>
2026-01-14 19:04:56 +0100

    refactor: SOLID guidelines

--- migasfree/stats/views/events.py
+++ migasfree/stats/views/events.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import time
+
 from datetime import date, datetime, timedelta
 
 from dateutil.relativedelta import relativedelta
@@ -26,6 +26,7 @@ from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action, permission_classes
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 
 from ...client.models import (
@@ -110,199 +111,125 @@ def month_interval(begin_month='', end_month=''):
     return first_day_month(begin_date), end_date
 
 
-def event_by_month(data, begin_date, end_date, model, field='project_id'):
-    labels = {}
-    new_data = {}
-    chart_data = {}
+class EventAggregator:
+    def __init__(self, data, model, field='project_id', dataset_label=None):
+        self.data_list = data
+        self.model = model
+        self.field_name = field
+        self.dataset_label = dataset_label or str(model._meta.verbose_name_plural)
+        self.categories = self._get_categories()
+        self.data_map = self._build_data_map()
 
-    if field == 'project_id':
-        projects = Project.objects.only('id', 'name', 'platform')
-        for project in projects:
-            new_data[project.id] = []
-            labels[project.id] = project.name
-    elif field == 'status':
-        for item in Computer.STATUS_CHOICES:
-            new_data[item[0]] = []
-            labels[item[0]] = _(item[1])
-    elif field == 'checked':
-        new_data[True] = []
-        new_data[False] = []
-        labels[True] = _('Checked')
-        labels[False] = _('Unchecked')
-
-    # shuffle data series
-    x_axe = []
-    for monthly in month_year_iter(begin_date.month, begin_date.year, end_date.month, end_date.year):
-        start_date = date(monthly[0], monthly[1], 1)
-        final_date = start_date + relativedelta(months=+1)
-
-        key = f'{monthly[0]}-{monthly[1]:02}'
-        x_axe.append(key)
-        value = list(filter(lambda item: item['year'] == monthly[0] and item['month'] == monthly[1], data))
-        if field == 'project_id':
+    def _get_categories(self):
+        categories = {}
+        if self.field_name is None:
+            return {'total': self.dataset_label}
+        elif self.field_name == 'project_id':
+            projects = Project.objects.only('id', 'name', 'platform')
             for project in projects:
-                if value:
-                    count = list(filter(lambda item: item['project_id'] == project.id, value))
-                    new_data[project.id].append(
-                        {
-                            'value': count[0]['count'] if count else 0,
-                            'model': model,
-                            'project__id__exact': project.id,
-                            'created_at__gte': start_date.strftime('%Y-%m-%d'),
-                            'created_at__lt': final_date.strftime('%Y-%m-%d'),
-                        }
-                    )
-                else:
-                    new_data[project.id].append(
-                        {
-                            'value': 0,
-                        }
-                    )
-        elif field == 'status':
+                categories[project.id] = project.name
+        elif self.field_name == 'status':
             for item in Computer.STATUS_CHOICES:
-                if value:
-                    count = list(filter(lambda row: row['status'] == item[0], value))
-                    new_data[item[0]].append(
-                        {
-                            'value': count[0]['count'] if count else 0,
-                            'model': model,
-                            'status__in': item[0],
-                            'created_at__gte': start_date.strftime('%Y-%m-%d'),
-                            'created_at__lt': final_date.strftime('%Y-%m-%d'),
-                        }
-                    )
-                else:
-                    new_data[item[0]].append(
-                        {
-                            'value': 0,
-                        }
-                    )
-        elif field == 'checked':
-            for val in [True, False]:
-                if value:
-                    count = list(filter(lambda item: item['checked'] == val, value))
-                    new_data[val].append(
-                        {
-                            'value': count[0]['count'] if count else 0,
-                            'model': model,
-                            'checked__exact': 1 if val else 0,
-                            'created_at__gte': start_date.strftime('%Y-%m-%d'),
-                            'created_at__lt': final_date.strftime('%Y-%m-%d'),
-                        }
-                    )
-                else:
-                    new_data[val].append(
-                        {
-                            'value': 0,
-                        }
-                    )
+                categories[item[0]] = _(item[1])
+        elif self.field_name == 'checked':
+            categories[True] = _('Checked')
+            categories[False] = _('Unchecked')
+        return categories
 
-    for item in new_data:
-        chart_data[labels[item]] = new_data[item]
+    def _build_data_map(self):
+        """
+        Builds a lookup dictionary from the data list.
+        Key: (year, month, day, hour, field_value)
+        """
+        mapping = {}
+        for item in self.data_list:
+            year = item.get('year')
+            month = item.get('month')
+            day = item.get('day', 1)
+            hour = item.get('hour', 0)
+            if hasattr(hour, 'hour'):
+                hour = hour.hour
 
-    return {'x_labels': x_axe, 'data': chart_data}
+            field_val = item.get(self.field_name) if self.field_name else 'total'
+
+            key = (year, month, day, hour, field_val)
+            mapping[key] = item['count']
+        return mapping
+
+    def aggregate(self, begin_date, end_date, frequency='month'):
+        chart_data = {}
+        new_data = {cat_id: [] for cat_id in self.categories}
+        x_axis = []
+
+        if frequency == 'month':
+            iterator = month_year_iter(begin_date.month, begin_date.year, end_date.month, end_date.year)
+        elif frequency == 'day':
+            iterator = self._daily_iterator(begin_date, end_date)
+        elif frequency == 'hour':
+            iterator = datetime_iterator(begin_date, end_date - timedelta(hours=1), delta=timedelta(hours=1))
+
+        for time_step in iterator:
+            if frequency == 'month':
+                year, month = time_step
+                start_date = date(year, month, 1)
+                final_date = start_date + relativedelta(months=+1)
+                key_label = f'{year}-{month:02}'
+                day = 1
+                hour = 0
+            elif frequency == 'day':
+                start_date = time_step
+                final_date = start_date + timedelta(days=1)
+                key_label = start_date.strftime('%Y-%m-%d')
+                year, month, day = start_date.year, start_date.month, start_date.day
+                hour = 0
+            elif frequency == 'hour':
+                start_date = time_step
+                final_date = start_date + timedelta(hours=1)
+                key_label = start_date.strftime('%Y-%m-%d %H:%M:%S')
+                year, month, day, hour = start_date.year, start_date.month, start_date.day, start_date.hour
+
+            x_axis.append(key_label)
+
+            for cat_id in self.categories:
+                lookup_key = (year, month, day, hour, cat_id if self.field_name else 'total')
+                count = self.data_map.get(lookup_key, 0)
+
+                entry = {
+                    'value': count,
+                    'model': self.model._meta.model_name,
+                    'created_at__gte': start_date.strftime('%Y-%m-%dT%H:%M:%S' if frequency == 'hour' else '%Y-%m-%d'),
+                    'created_at__lt': final_date.strftime('%Y-%m-%dT%H:%M:%S' if frequency == 'hour' else '%Y-%m-%d'),
+                }
+
+                if self.field_name == 'project_id':
+                    entry['project__id__exact'] = cat_id
+                elif self.field_name == 'status':
+                    entry['status__in'] = cat_id
+                elif self.field_name == 'checked':
+                    entry['checked__exact'] = 1 if cat_id else 0
+
+                new_data[cat_id].append(entry)
+
+        for cat_id, cat_label in self.categories.items():
+            chart_data[cat_label] = new_data[cat_id]
+
+        return {'x_labels': x_axis, 'data': chart_data}
+
+    def _daily_iterator(self, begin_date, end_date):
+        current_date = begin_date
+        while current_date <= end_date:
+            yield current_date
+            current_date += timedelta(days=1)
+
+
+def event_by_month(data, begin_date, end_date, model, field='project_id'):
+    aggregator = EventAggregator(data, model, field)
+    return aggregator.aggregate(begin_date, end_date, frequency='month')
 
 
 def event_by_day(data, begin_date, end_date, model, field='project_id'):
-    labels = {}
-    new_data = {}
-    chart_data = {}
-
-    if field == 'project_id':
-        projects = Project.objects.only('id', 'name', 'platform')
-        for project in projects:
-            new_data[project.id] = []
-            labels[project.id] = project.name
-    elif field == 'status':
-        for item in Computer.STATUS_CHOICES:
-            new_data[item[0]] = []
-            labels[item[0]] = _(item[1])
-    elif field == 'checked':
-        new_data[True] = []
-        new_data[False] = []
-        labels[True] = _('Checked')
-        labels[False] = _('Unchecked')
-
-    date_range = []
-    current_date = begin_date
-    while current_date <= end_date:
-        date_range.append(current_date)
-        current_date += timedelta(days=1)
-
-    x_axe = [date.strftime('%Y-%m-%d') for date in date_range]
-
-    for date_item in date_range:
-        start_date = date_item
-        end_of_day = date_item + timedelta(days=1)  # range [start, end)
-
-        if field == 'project_id':
-            for project in projects:
-                value = list(
-                    filter(
-                        lambda item: item['year'] == start_date.year
-                        and item['month'] == start_date.month
-                        and item['day'] == start_date.day,
-                        data,
-                    )
-                )
-
-                count = list(filter(lambda item: item['project_id'] == project.id, value))
-                new_data[project.id].append(
-                    {
-                        'value': count[0]['count'] if count else 0,
-                        'model': model,
-                        'project__id__exact': project.id,
-                        'created_at__gte': start_date.strftime('%Y-%m-%d'),
-                        'created_at__lt': end_of_day.strftime('%Y-%m-%d'),
-                    }
-                )
-        elif field == 'status':
-            for item in Computer.STATUS_CHOICES:
-                value = list(
-                    filter(
-                        lambda item: item['year'] == start_date.year
-                        and item['month'] == start_date.month
-                        and item['day'] == start_date.day,
-                        data,
-                    )
-                )
-
-                count = list(filter(lambda row: row['status'] == item[0], value))
-                new_data[item[0]].append(
-                    {
-                        'value': count[0]['count'] if count else 0,
-                        'model': model,
-                        'status__in': item[0],
-                        'created_at__gte': start_date.strftime('%Y-%m-%d'),
-                        'created_at__lt': end_of_day.strftime('%Y-%m-%d'),
-                    }
-                )
-        elif field == 'checked':
-            for val in [True, False]:
-                value = list(
-                    filter(
-                        lambda item: item['year'] == start_date.year
-                        and item['month'] == start_date.month
-                        and item['day'] == start_date.day,
-                        data,
-                    )
-                )
-
-                count = list(filter(lambda item: item['checked'] == val, value))
-                new_data[val].append(
-                    {
-                        'value': count[0]['count'] if count else 0,
-                        'model': model,
-                        'checked__exact': 1 if val else 0,
-                        'created_at__gte': start_date.strftime('%Y-%m-%d'),
-                        'created_at__lt': end_of_day.strftime('%Y-%m-%d'),
-                    }
-                )
-
-    for item in new_data:
-        chart_data[labels[item]] = new_data[item]
-
-    return {'x_labels': x_axe, 'data': chart_data}
+    aggregator = EventAggregator(data, model, field)
+    return aggregator.aggregate(begin_date, end_date, frequency='day')
 
 
 @extend_schema(tags=['stats'])
@@ -321,7 +248,7 @@ class EventViewSet(viewsets.ViewSet):
             if pattern in self.basename:
                 return event_class
 
-        raise ValueError('No matching event class found')
+        raise NotFound('No matching event class found')
 
     @extend_schema(parameters=[computer_id, start_date, end_date])
     @action(methods=['get'], detail=False, url_path='by-day')
@@ -355,8 +282,6 @@ class EventViewSet(viewsets.ViewSet):
         user = request.user.userprofile
         now = timezone.localtime(timezone.now())
         fmt = '%Y-%m-%dT%H'
-        human_fmt = '%Y-%m-%d %H:%M:%S'
-        value_fmt = '%Y-%m-%dT%H:%M:%S'
 
         end = request.query_params.get('end', '')
         try:
@@ -371,23 +296,20 @@ class EventViewSet(viewsets.ViewSet):
             begin = end - timedelta(days=settings.HOURLY_RANGE)
 
         event_class = self.get_event_class()
-        events = {i['hour'].strftime(human_fmt): i['count'] for i in event_class.by_hour(begin, end, user)}
+        data = event_class.by_hour(begin, end, user)
 
-        labels = []
-        stats = []
+        # Normalize data for EventAggregator
+        # by_hour returns dicts with 'hour': datetime, 'count': int
+        data_list = []
+        for item in data:
+            dt = item['hour']
+            # Defensive check if dt is string (though usually datetime from queryset)
+            if isinstance(dt, str):
+                dt = timezone.make_aware(datetime.strptime(dt, fmt))
 
-        for item in datetime_iterator(begin, end - timedelta(hours=1), delta=timedelta(hours=1)):
-            next_item = item + timedelta(hours=1)
-            labels.append(time.strftime(human_fmt, item.timetuple()))
-            stats.append(
-                {
-                    'model': event_class._meta.model_name,
-                    'created_at__gte': time.strftime(value_fmt, item.timetuple()),
-                    'created_at__lt': time.strftime(value_fmt, next_item.timetuple()),
-                    'value': events.get(str(item), 0),
-                }
+            data_list.append(
+                {'year': dt.year, 'month': dt.month, 'day': dt.day, 'hour': dt.hour, 'count': item['count']}
             )
 
-        return Response(
-            {'x_labels': labels, 'data': {str(event_class._meta.verbose_name_plural): stats}}, status=status.HTTP_200_OK
-        )
+        aggregator = EventAggregator(data_list, event_class, field=None)
+        return Response(aggregator.aggregate(begin, end, frequency='hour'), status=status.HTTP_200_OK)
